c6bdff70bea837fdec42818d5bce2818cb4424de
David Kobia <david@ushahidi.com>
2009-03-03 13:09:26 -0500

    * Removed one more line from sql script to close #69

--- sql/ushahidi.sql
+++ sql/ushahidi.sql
@@ -833,8 +833,6 @@ CREATE TABLE IF NOT EXISTS `settings` (
 -- Dumping data for table `settings`
 --
 
-ALTER TABLE `settings` ADD COLUMN `twitter_hashtags` TEXT NULL DEFAULT NULL AFTER `google_analytics`;
-
 INSERT INTO `settings` (`id`, `site_name`, `default_map`, `api_google`, `api_yahoo`, `api_live`, `default_country`, `default_city`, `default_lat`, `default_lon`, `default_zoom`, `items_per_page`, `items_per_page_admin`, `date_modify`) VALUES
 (1, 'Ushahidi Beta', 1, 'ABQIAAAAjsEM5UsvCPCIHp80spK1kBQKW7L4j6gYznY0oMkScAbKwifzxxRhJ3SP_ijydkmJpN3jX8kn5r5fEQ', '5CYeWbfV34E21JOW1a4.54Mf6e9jLNkD0HVzaKoQmJZi2qzmSZd5mD8X49x7', NULL, 115, 'nairobi', '-1.2873000707050097', '36.821451182008204', 13, 20, 20, '2008-08-25 10:25:18');
 
