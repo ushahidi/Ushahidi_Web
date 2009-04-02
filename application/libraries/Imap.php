184ea1e9eb5e4747caec1950196f3ae0ce010834
Jason Mule <jason@wanderingelf.(none)>
2009-04-02 23:52:30 +0300

    Added support for decoding mime header elements and quoted-printable strings

--- application/libraries/Imap.php
+++ application/libraries/Imap.php
@@ -2,7 +2,7 @@
 /**
  * Imap library. Provides an IMAP interface to mailboxes. 
  * @package    Imap
- * @author     Ushahidi Team
+ * @author	   Ushahidi Team
  * @copyright  (c) 2009 Ushahidi Team
  * @license    http://www.ushahidi.com/license.html
  */
@@ -16,64 +16,66 @@ class Imap_Core {
 	public function __construct($config = array())
 	{
 		// Use default imap configuration when none is provided
-        $config = !empty($config) ? $config : Kohana::config('imap');
-        
-        $imap_stream =  imap_open($config['service'], $config['email_address'], 
-                           $config['password']);
+		$config = !empty($config) ? $config : Kohana::config('imap');
+		
+		$imap_stream =	imap_open($config['service'], $config['email_address'], 
+						   $config['password']);
 
 		$this->imap_stream = $imap_stream;
 	}
 
-    /**
-     * Get messages according to a search criteria
-     * 
-     * @param   string  search criteria (RFC2060, sec. 6.4.4). Set to "UNSEEN" by default
-     * @param   string  date format. Set to "Y-m-d H:i:s" by default
-     * @return  mixed   array containing messages
-     */
-    public function get_messages($search_criteria="UNSEEN", 
-                                 $date_format="Y-m-d H:i:s")
-    {
-        $msgs = imap_search($this->imap_stream, $search_criteria);
-        $no_of_msgs = $msgs ? count($msgs) : 0;
+	/**
+	 * Get messages according to a search criteria
+	 * 
+	 * @param	string	search criteria (RFC2060, sec. 6.4.4). Set to "UNSEEN" by default
+	 * @param	string	date format. Set to "Y-m-d H:i:s" by default
+	 * @return	mixed	array containing messages
+	 */
+	public function get_messages($search_criteria="UNSEEN", 
+								 $date_format="Y-m-d H:i:s")
+	{
+		$msgs = imap_search($this->imap_stream, $search_criteria);
+		$no_of_msgs = $msgs ? count($msgs) : 0;
 
-        print Kohana::debug($no_of_msgs);
+		$messages = array();
 
-        $messages = array();
-
-        for ($i = 0; $i < $no_of_msgs; $i++) 
+		for ($i = 0; $i < $no_of_msgs; $i++) 
 		{
-            $header = imap_header($this->imap_stream, $msgs[$i]);
-            $date = date($date_format, $header->udate);
-            $from = $header->fromaddress;
-            $subject = $header->subject;
+			$header = imap_header($this->imap_stream, $msgs[$i]);
+			$date = date($date_format, $header->udate);
+			$from = $this->_mime_decode($header->fromaddress);
+			$subject = $this->_mime_decode($header->subject);
 
-            $structure = imap_fetchstructure($this->imap_stream, $msgs[$i]);
+			$structure = imap_fetchstructure($this->imap_stream, $msgs[$i]);
 
-            if (!empty($structure->parts))
-            {
+			if (!empty($structure->parts))
+			{
 				for ($j = 0, $k= count($structure->parts); $j < $k; $j++)
-                {
-                    $part = $structure->parts[$j];
+				{
+					$part = $structure->parts[$j];
 
-                    if ($part->subtype == 'PLAIN')
-                    {
-                    	$body = imap_fetchbody($this->imap_stream, $msgs[$i], $j+1);
+					if ($part->subtype == 'PLAIN')
+					{
+						$body = imap_fetchbody($this->imap_stream, $msgs[$i], $j+1);
 					}
 				}
 			}
-			else
+			else {
 				$body = imap_body($this->imap_stream, $msgs[$i]);
-			
-            array_push($messages, array('msg_no' => $msgs[$i],
-										'date' => $date,
-                                        'from' => $from,
-                                        'subject' => $subject,
-                                        'body' => $body));
-        }
+			}
 
-        return $messages;
-    }
+			// Convert quoted-printable strings (RFC2045)
+			$body = imap_qprint($body);
+			
+			array_push($messages, array('msg_no' => $msgs[$i],
+										'date' => $date,
+										'from' => $from,
+										'subject' => $subject,
+										'body' => $body));
+		}
+
+		return $messages;
+	}
 
 	/**
 	 * Delete a message
@@ -84,12 +86,24 @@ class Imap_Core {
 		imap_delete($this->imap_stream, $msgs[$i]);
 	}
 
-    /**
-     * Closes an IMAP stream
-     */
-    public function close()
-    {
-        imap_close($this->imap_stream);
-    }
+	/**
+	 * Closes an IMAP stream
+	 */
+	public function close()
+	{
+		imap_close($this->imap_stream);
+	}
+
+	private function _mime_decode($str) 
+	{
+		$elements = imap_mime_header_decode($str);
+		$text = "";
 		
+		foreach ($elements as $element) 
+		{
+			$text.= $element->text;
+		}
+
+		return $text;
+	}
 } // End Imap
