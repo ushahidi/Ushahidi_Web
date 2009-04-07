60f513d99e3e5e8e2e4003b6b44906b735a76674
Henry Addo <eyedol@addhen.(none)>
2009-04-07 16:33:07 +0000

    Fixed non-existing db select with the installer

--- installer/install.php
+++ installer/install.php
@@ -29,7 +29,7 @@ class Install
 
 	public function index()
 	{
-	   
+	   session_start();
 	}
 
 	/**
@@ -186,12 +186,12 @@ class Install
 	private function import_sql($username, $password, $host,$db_name)
 	{
 	    $connection = @mysql_connect("$host", "$username", "$password");
-		@mysql_select_db("$db_name",$connection);
-
 	    $db_schema = file_get_contents('../sql/ushahidi.sql');
 
-	    $resutl = @mysql_query('CREATE DATABASE '.$db_name);
-
+	    $result = @mysql_query('CREATE DATABASE '.$db_name);
+	    
+	    // select newly created db
+	    @mysql_select_db($db_name,$connection);
 	    /**
 	     * split by ; to get the sql statement for creating individual
 	     * tables.
