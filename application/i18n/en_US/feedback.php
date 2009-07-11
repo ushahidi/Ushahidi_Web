1a19f5a0aa274a6a644329baab45ebfca7497028
eyedol <addhen@linuxmail.org>
2009-07-11 20:59:27 +0000

    Initial feedback implementation

---
+++ application/i18n/en_US/feedback.php
@@ -0,0 +1,62 @@
+<?php
+	$lang = array
+	(
+		'feedback' => 'Feedback',
+		
+		'show_all' => 'Show all',
+		
+		'read' => 'READ',
+		
+		'unread' => 'UNREAD',
+		
+		'delete' => 'DELETE',
+		
+		'error_title' => 'Error!',
+		
+		'error_msg' => 'Please verify that you have checked an item',
+		
+		'hide_msg' => 'hide this message',
+		
+		'feedback_details' => 'Feedback Details',
+		
+		'feedback_date' => 'Date',
+		
+		'feedback_actions' => 'Actions',
+		
+		'feedback_no_result' => 'No Feedback To Display!',
+		
+		'feedback_title' => array
+		(
+			'required' => 'Please enter the name of the feed.',
+			'length'   => 'The feed name field must be at least 3 and no more 
+				70 characters long.'
+		),
+		
+		'feedback_message' => array
+		(
+			'required' => 'some message.',
+		),
+		
+		'person_name' => array
+		(
+			'required' => 'Please enter feed\'s URL.',
+			'url' => 'Please enter a valid URL. Eg. http://www.ushahidi.com'
+		),
+		
+		'person_email' => array
+		(
+			'required' => 'Please enter feed\'s URL.',
+			'url' => 'Please enter a valid URL. Eg. http://www.ushahidi.com'
+		),
+		
+		
+		'feedback_captcha' => array
+		(
+			'required' => 'some message.',
+			'captcha' => 'some message.'
+		)
+		
+	);
+
+?>
+
--- application/views/admin/feedback.php
+++ application/views/admin/feedback.php
@@ -14,26 +14,113 @@
  */
 ?>
 <div class="bg">
-	<h2><?php echo $title; ?></h2>
-	<div class="table-holder">
-		<span class="big_blue_span">Installation</span>
-		<p>
-			This is some descriptive text that talks about Installation feedback.  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed.
-			<p>
-				<a href="http://feedback.ushahidi.com/fillsurvey.php?sid=5" class="no_border">
-					<img src="<?php echo url::base() ?>media/img/admin/download_frontline_engine.gif" />
-				</a>
-			</p>
-		</p>
-		<span class="big_blue_span">Post Implementation</span>
-		<p>
-			This is some descriptive text that talks about Post Implementation feedback.  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed.
-			<p>
-				<a href="http://feedback.ushahidi.com/fillsurvey.php?sid=6" class="no_border">
-					<img src="<?php echo url::base() ?>media/img/admin/download_frontline_engine.gif" />
-				</a>
-			</p>
-		</p>
+	<h2>
+		<?php echo Kohana::lang('feedback.feedback'); ?> 
+		<span>
+			(<?php echo "";//$total_items; ?>)
+		</span>
+	</h2>
+	<!-- tabs -->
+	<div class="tabs">
+		<!-- tabset -->
+		<ul class="tabset">
+			<li><a href="?status=0" <?php //if ($status != 'a' && $status !='v') 
+				echo "class=\"active\""; ?>>
+				<?php echo Kohana::lang('feedback.show_all'); ?> </a>
+			</li>
+		</ul>
+		<!-- tab -->
+		<div class="tab">
+			<ul>
+				<li><a href="#" onclick="reportAction('r','READ', '');">
+					<?php echo Kohana::lang('feedback.read'); ?></a>
+				</li>
+				
+				<li><a href="#" onclick="reportAction('u','UNREAD', '');">
+					<?php echo Kohana::lang('feedback.unread'); ?></a>
+				</li>
+				
+				<li><a href="#" onclick="reportAction('d','DELETE', '');">
+					<?php echo Kohana::lang('feedback.delete'); ?></a>
+				</li>
+			</ul>
+		</div>
 	</div>
+	<?php
+	//if ($form_error) {
+	?>
+		<!-- red-box -->
+		<div class="red-box">
+			<h3><?php echo Kohana::lang('feedback.error_title'); ?></h3>
+			<ul><?php echo Kohana::lang('feedback.error_msg'); ?></ul>
+		</div>
+	<?php
+	//}
+
+	//if ($form_saved) {
+	?>
+		<!-- green-box -->
+		<div class="green-box" id="submitStatus">
+			<h3>
+				<?php echo Kohana::lang('feedback.feedback'); ?>
+				 <?php echo "";//$form_action; ?> 
+				<a href="#" id="hideMessage" class="hide">
+					<?php echo Kohana::lang('feedback.hide_msg'); ?></a>
+			</h3>
+		</div>
+	<?php
+	//}
+	?>
+	<!-- report-table -->
+	<?php print form::open(NULL, array('id' => 'feedbackMain', 'name' => 'feedbackMain')); ?>
+		<input type="hidden" name="action" id="action" value="">
+		<input type="hidden" name="incident_id[]" id="incident_single" value="">
+		<div class="table-holder">
+			<table class="table">
+				<thead>
+					<tr>
+						<th class="col-1">
+							<input id="checkallincidents" type="checkbox" 
+							class="check-box" 
+							onclick="CheckAll( this.id, 'incident_id[]' )" />
+						</th>
+						<th class="col-2">
+							<?php echo Kohana::lang('feedback.feedback_details'); ?>
+						</th>
+						<th class="col-3">
+							<?php echo Kohana::lang('feedback.feedback_date'); ?>
+						</th>
+						<th class="col-4">
+							<?php echo Kohana::lang('feedback.feedback_actions'); ?>
+						</th>
+					</tr>
+				</thead>
+				<tfoot>
+					<tr class="foot">
+						<td colspan="4">
+							<?php echo "";//$pagination; ?>
+						</td>
+					</tr>
+				</tfoot>
+				<tbody>
+					<?php
+					//if ($total_items == 0)
+					{
+					?>
+						<tr>
+							<td colspan="4" class="col">
+								<h3>
+									<?php echo Kohana::lang('feedback.feedback_no_result'); ?>
+								</h3>
+							</td>
+						</tr>
+					<?php	
+					}
+				
+					?>
+				</tbody>
+			</table>
+		</div>
+	<?php print form::close(); ?>
 </div>
 
