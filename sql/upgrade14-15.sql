6aa033e83447cf377baff47be158a6daeba574fc
Brian Herbert <brianherbert@gmail.com>
2009-11-02 13:50:10 -0500

    Database version numbers need to be advanced to 15.

--- sql/upgrade14-15.sql
+++ sql/upgrade14-15.sql
@@ -9,3 +9,5 @@ CREATE TABLE IF NOT EXISTS `page` (
 
 INSERT INTO `page` (`id`, `page_title`, `page_description`, `page_tab`, `page_active`) VALUES
 (1, 'About Us', '<p>This is the default about us page.</p>', 'About Us', 1);
+
+UPDATE `settings` SET `db_version` = '15' WHERE `id` =1 LIMIT 1 ;
