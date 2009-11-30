484d35ba0e859ecd792cbd489b17da331ff0513c
Soyapi Mumba <soyapim@gmail.com>
2009-11-30 07:19:09 +0200

    Added incident_alert_status for the default incident

--- sql/ushahidi.sql
+++ sql/ushahidi.sql
@@ -398,7 +398,7 @@ CREATE TABLE IF NOT EXISTS `incident` (
 
 LOCK TABLES `incident` WRITE;
 /*!40000 ALTER TABLE `incident` DISABLE KEYS */;
-INSERT INTO `incident` VALUES (1,1,1,'en_US',1,'Hello Ushahidi!','Welcome to Ushahidi. Please replace this report with a valid incident','2009-06-30 12:00:00',1,1,1,'0','2009-06-30 12:00:00','0',NULL,NULL,NULL);
+INSERT INTO `incident` VALUES (1,1,1,'en_US',1,'Hello Ushahidi!','Welcome to Ushahidi. Please replace this report with a valid incident','2009-06-30 12:00:00',1,1,1,'0','2009-06-30 12:00:00','0',NULL,NULL,NULL,'0');
 /*!40000 ALTER TABLE `incident` ENABLE KEYS */;
 UNLOCK TABLES;
 
