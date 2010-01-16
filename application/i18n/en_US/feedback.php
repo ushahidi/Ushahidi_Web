e053e7a314384319908d08e2ea4c697a2231cb2a
Pablo Destefanis <malasanha@gmail.com>
2010-01-16 02:02:30 -0500

    Adding missing strings to i18n/en_US and i18n/es_AR (cherry picked from commit f9169406416ee4cf51b4a68b0fcade6f6f89c41f)

--- application/i18n/en_US/feedback.php
+++ application/i18n/en_US/feedback.php
@@ -1,39 +1,10 @@
 <?php
 $lang = array
 (
-	'feedback' => 'Feedback',
-	
-	'show_all' => 'All',
-	
-	'read' => 'READ',
-	
-	'unread' => 'UNREAD',
-	
-	'delete' => 'DELETE',
-	
-	'error_title' => 'Error!',
-	
-	'error_msg' => 'Please verify that you have checked an item',
-	
-	'hide_msg' => 'hide this message',
-	
 	'feedback_details' => 'Feedback Details',
-	
 	'feedback_date' => 'Date',
-	
 	'feedback_actions' => 'Actions',
-	
-	'feedback_no_result' => 'No Feedback To Display!',
-	
-	'feedback_page_title' => 'Feedback Details',
-	'feedback_reply_title' => 'Title',
-	'feedback_reply_send' => 'Send',
-	'feedback_reply_message' => 'Message Details',
-	'feedback_person_name' => 'Name',
-	'feedback_person_email' => 'Email',
-	'feedback_person_ip' => 'IP',
-	'feedback_send_reply' => 'Send Reply',
-	'feedback_confirm_mesg' => 'Message sent',		
+			
 	'feedback_title' => array
 	(
 		'required'		=> 'A title is required.',
--- application/i18n/en_US/settings.php
+++ application/i18n/en_US/settings.php
@@ -1,7 +1,86 @@
 <?php
 
+/**
+ * SETTINGS INTERFACE INTERNATIONALIZATION
+ *
+ * en_US
+ */
+
 $lang = array
 (
+    'default_location' => 'Default Location',
+    'select_default_location' => 'Please select a default country',
+    'download_city_list' => 'Retrieve Cities From Geonames',
+    'multiple_countries' => 'Does this Ushahidi Instance Span Multiple Countries', 
+    'map_settings' => 'Map Settings',
+    'configure_map' => 'Configure Map',
+    'default_map_view' => 'Default Map View',
+    'default_zoom_level' => 'Default Zoom Level',
+    'set_location' => 'Click and drag the map to set your exact location',
+    'map_provider' => array
+	(
+		'name'		=> 'Map provider',
+		'info'		=> 'Setting up your map provider is a straight- forward process. Select a provider, obtain an API key from the provider\'s site, and enter the API key',
+        'choose'    => 'Select a Map Provider',
+        'get_api'   => 'Get an API Key',
+        'enter_api' => 'Enter the new API Key'
+	),
+    'site' => array
+	(
+		'display_contact_page' => 'Display Contact Page',
+        'display_howtohelp_page' => 'Display "How to Help" Page',
+        'email_alerts' => 'Alerts Email Address',
+        'email_notice' => '<span>In order to receive reports by email, please <a href="'.url::base().'admin/settings/email">configure your email account settings</a>.</span>',
+        'email_site' => 'Site Email Address',
+        'title'		=> 'Site Settings',
+		'name'		=> 'Site Name',
+        'tagline'    => 'Site Tagline',
+        'items_per_page'   => 'Items Per Page - Front End',
+        'items_per_page_admin' => 'Items Per Page - Admin',
+        'allow_reports' => 'Allow Users To Submit Reports',
+        'allow_comments' => 'Allow Users to Submit Comments to Reports',
+        'allow_feed' => 'Include RSS News Feed on Website',
+        'allow_clustering' => 'Cluster Reports on Map',
+        'default_category_colors' => 'Default Color For All Categories',
+        'google_analytics' => 'Google Analytics',
+        'google_analytics_example' => 'Web Property ID - Formato: UA-XXXXX-XX',
+        'twitter_configuration' => 'Twitter Credentials',
+        'twitter_hashtags' => 'Hashtags - Separate with commas ',
+        'laconica_configuration' => 'Laconica Credentials',
+        'laconica_site' => 'Laconica Site ',
+        'language' => 'Site Language',
+        'api_akismet' => 'Akismet Key',
+        'kismet_notice' => 'Prevent comment spam using <a href="http://akismet.com/" target="_blank">Akismet</a> from Automattic. <BR />You can get a free API key by registering for a <a href="http://en.wordpress.com/api-keys/" target="_blank">WordPress.com user account</a>',
+        'share_site_stats' => 'Share Site Statistics in API'
+	),    
+    'sms' => array
+	(
+        'title' => 'SMS Setup Options',
+        'option_1'		=> 'Option 1: Use Frontline SMS', 
+		'option_2'		=> 'Option 2: Use a Global SMS Gateway',
+        'flsms_description' => 'FrontlineSMS is free open source software that turns a laptop and a mobile phone into a central communications hub. Once installed, the program enables users to send and receive text messages with large groups of people through mobile phones. Click on the grey box to request a download from FrontlineSMS.com',
+        'flsms_download'    => 'Download Frontline SMS and install it on your computer',
+        'flsms_synchronize'   => 'Sync with Ushahidi',
+        'flsms_instructions' => 'Messages received into a FrontlineSMS hub can be synched with Ushahidi. Detailed instructions on how to sync can be found <strong><a href="http://wiki.ushahididev.com/doku.php?id=how_to_use_ushahidi_alpha#how_to_setup_frontlinesms_to_sync_with_ushahidi" target="_blank">here</a></strong>. You will require the key and link below to set up the sync with FrontlineSMS',
+        'flsms_key' => 'Your Ushahidi Sync Key',
+        'flsms_link' => 'FrontlineSMS HTTP Post LINK', 
+        'flsms_text_1' => 'Enter phone number(s) connected to Frontline SMS in the field(s) below',
+        'flsms_text_2' => 'Enter the number without any + or dashes below',
+        'clickatell_text_1' => 'Sign up for Clickatells service by <a href="https://www.clickatell.com/central/user/client/step1.php?prod_id=2" target="_blank">clicking here</a>',
+        'clickatell_text_2' => 'Enter your Clickatell access information below',
+        'clickatell_api' => 'Your Clickatell API Number',
+        'clickatell_username' => 'Your Clickatell User Name', 
+        'clickatell_password' => 'Your Clickatell Password', 
+        'clickatell_check_balance' => 'Check Your Clickatell Credit Balance', 
+        'clickatell_load_balance' => 'Load Credit Balance'
+	),      
+    
+    'map' => array
+	(
+		'zoom'		=> 'Zoom Level',
+		'default_location'	=> 'Setting up your map provider is a straight- forward process. Select a provider, obtain an API key from the provider\'s site, and enter the API key'
+	),   
+    
 	'site_name' => array
 	(
 		'required'		=> 'The site name field is required.',
