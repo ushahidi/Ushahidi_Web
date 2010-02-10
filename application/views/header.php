2e2329e62812293afe6d2cbffd206b2a910f89af
Max Froumentin <mf@acer.(none)>
2010-02-10 19:40:41 +0100

    replace photoslider with picbox (2)

--- application/views/header.php
+++ application/views/header.php
@@ -58,8 +58,8 @@
 	}
 	
 	if ($photoslider_enabled) {
-		echo html::script('media/js/photoslider');
-		echo html::stylesheet('media/css/photoslider');
+		echo html::script('media/js/picbox', true);
+		echo html::stylesheet('media/css/picbox/picbox');
 	}
 	
 	if( $videoslider_enabled ) {
