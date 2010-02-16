162b4d245426fe8ab15cb4cd16058834cd3951d5
Henry Addo <eyedol@addhen.(none)>
2010-02-16 13:31:47 +0000

    updated version for db

--- sql/ushahidi.sql
+++ sql/ushahidi.sql
@@ -1358,6 +1358,31 @@ CREATE TABLE `layer` (
 ) ENGINE = MYISAM  DEFAULT CHARSET=utf8;
 
 --
+-- Table structure for table `api_banned`
+--
+
+CREATE TABLE IF NOT EXISTS `api_banned` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `banned_ipaddress` varchar(50) NOT NULL,
+  `banned_date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='For logging banned API IP addresses' AUTO_INCREMENT=8 ;
+
+--
+-- Table structure for table `api_log`
+--
+
+CREATE TABLE IF NOT EXISTS `api_log` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `api_task` varchar(10) NOT NULL,
+  `api_parameters` varchar(50) NOT NULL,
+  `api_records` tinyint(11) NOT NULL,
+  `api_ipaddress` varchar(50) NOT NULL,
+  `api_date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='For logging API activities' AUTO_INCREMENT=19 ;
+
+--
 -- Constraints for dumped tables
 --
 
@@ -1386,10 +1411,8 @@ ALTER TABLE `form_response`
 ALTER TABLE `user_tokens`
   ADD CONSTRAINT `user_tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
+  
 --
 -- Version information for table `settings`
 --
-UPDATE `settings` SET
-`db_version` = '21',
-`ushahidi_version` = '1.0.1'
-WHERE `id` =1 LIMIT 1;
+UPDATE `settings` SET `db_version` = '22' WHERE `id`=1 LIMIT 1;
