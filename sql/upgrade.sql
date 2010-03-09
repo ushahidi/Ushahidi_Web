1e5d4ed93b249734843250d09b4de6a1c3a042a9
Brian Herbert <brian@ushahidi.com>
2010-03-09 10:01:20 -0500

    Upgrade SQL for MHI from DB v22-23

--- sql/upgrade.sql
+++ sql/upgrade.sql
@@ -181,6 +181,79 @@ INSERT INTO `service` (`id`, `service_name`, `service_description`, `service_url
 (3, 'Twitter', 'Tweets tweets tweets', 'http://twitter.com', NULL),
 (4, 'Laconica', 'Tweets tweets tweets', NULL, NULL);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mhi_category`
+--
+
+CREATE TABLE `mhi_category` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) unsigned DEFAULT NULL,
+  `category_title` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `category_active` tinyint(4) DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mhi_site`
+--
+
+CREATE TABLE `mhi_site` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `site_domain` varchar(100) NOT NULL,
+  `site_privacy` tinyint(4) NOT NULL DEFAULT '0',
+  `site_active` tinyint(4) DEFAULT '1',
+  `site_dateadd` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mhi_site_category`
+--
+
+CREATE TABLE `mhi_site_category` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `site_id` int(11) unsigned NOT NULL,
+  `category_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mhi_site_database`
+--
+
+CREATE TABLE `mhi_site_database` (
+  `mhi_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `pass` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `host` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `port` smallint(6) NOT NULL,
+  `database` varchar(30) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`mhi_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 COMMENT='This table holds DB credentials for MHI instances';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mhi_users`
+--
+
+CREATE TABLE `mhi_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `firstname` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `lastname` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(40) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
