c5b0e645ef2d49b98e4967924fc0b20c8a3e63d3
Brian Herbert <brian@ushahidi.com>
2010-03-24 12:29:55 -0400

    Bringing MHI controller up to coding standards.

--- application/controllers/mhi.php
+++ application/controllers/mhi.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 /**
- * Contact Us Controller
+ * MHI Controller
  *
  * PHP version 5
  * LICENSE: This source file is subject to LGPL license
@@ -13,161 +13,179 @@
  * @license    http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
  */
 
-class MHI_Controller extends Template_Controller
-{
+class MHI_Controller extends Template_Controller {
 
 	// MHI template
-    public $template = 'layout';
+
+	public $template = 'layout';
 
 	function __construct()
-    {
-        parent::__construct();
+	{
+		parent::__construct();
 
-        // Load Header & Footer
-        $this->template->header  = new View('mhi_header');
-        $this->template->footer  = new View('mhi_footer');
+		// Load Header & Footer
 
-        $this->template->header->site_name = Kohana::config('settings.site_name');
+		$this->template->header  = new View('mhi_header');
+		$this->template->footer  = new View('mhi_footer');
 
-        // If we aren't at the top level MHI site or MHI isn't enabled, don't allow access to any of this jazz
-        if(Kohana::config('config.enable_mhi') == FALSE || Kohana::config('settings.subdomain') != '') {
-        	throw new Kohana_User_Exception('MHI Access Error', "MHI disabled for this site.");
-        }
-    }
+		$this->template->header->site_name = Kohana::config('settings.site_name');
 
-    public function index()
-    {
-    	$this->template->header->this_page = 'mhi';
-        $this->template->content = new View('mhi');
+		// If we aren't at the top level MHI site or MHI isn't enabled, don't allow access to any of this jazz
 
-        $session = Session::instance();
-        $mhi_user_id = $session->get('mhi_user_id');
+		if (Kohana::config('config.enable_mhi') == FALSE OR Kohana::config('settings.subdomain') != '')
+			throw new Kohana_User_Exception('MHI Access Error', "MHI disabled for this site.");
+	}
+
+	public function index()
+	{
+		$this->template->header->this_page = 'mhi';
+		$this->template->content = new View('mhi');
+
+		$session = Session::instance();
+		$mhi_user_id = $session->get('mhi_user_id');
 
 		$form = array(
 			'username' => '',
 			'password' => '',
 			);
 
-		// copy the form as errors, so the errors will be stored with keys corresponding to the form field names
+		// Copy the form as errors, so the errors will be stored with keys corresponding to the form field names
+
 		$errors = $form;
 		$form_error = FALSE;
 
 		// Set up the validation object
+
 		$_POST = Validation::factory($_POST)
 			->pre_filter('trim')
 			->add_rules('username', 'required')
 			->add_rules('password', 'required');
 
-		if ($_POST->validate() || $mhi_user_id != FALSE) {
+		if ($_POST->validate() OR $mhi_user_id != FALSE)
+		{
 			// Sanitize $_POST data removing all inputs without rules
-            $postdata_array = $_POST->safe_array();
 
-            // MHI user not already logged in, so do it
-            if($mhi_user_id == FALSE) {
-            	$mhi_user = new Mhi_User_Model();
-            	$mhi_user_id = $mhi_user->login($postdata_array['username'],$postdata_array['password']);
-            }
+			$postdata_array = $_POST->safe_array();
 
-            // If success (already logged in or login successful), move on
-            if($mhi_user_id != FALSE){
+			// MHI user not already logged in, so do it
 
-            	url::redirect('mhi/manage');
+			if ($mhi_user_id == FALSE)
+			{
+				$mhi_user = new Mhi_User_Model;
+				$mhi_user_id = $mhi_user->login($postdata_array['username'],$postdata_array['password']);
+			}
 
-            }else{
+			// If success (already logged in or login successful), move on
 
-            	echo 'FAILED LOGIN!';
+			if ($mhi_user_id != FALSE)
+			{
 
-	            $_POST->add_error('username', 'Login Error');
+				url::redirect('mhi/manage');
 
-	            // repopulate the form fields
-	            $form = arr::overwrite($form, $_POST->as_array());
+			}else{
 
-	            // populate the error fields, if any
-	            // We need to already have created an error message file, for Kohana to use
-	            // Pass the error message file name to the errors() method
-	            $errors = arr::overwrite($errors, $_POST->errors('auth'));
-	            $form_error = TRUE;
+				$_POST->add_error('username', 'Login Error');
+
+				// Repopulate the form fields
+
+				$form = arr::overwrite($form, $_POST->as_array());
+
+				// Populate the error fields, if any
+				// We need to already have created an error message file, for Kohana to use
+				// Pass the error message file name to the errors() method
+
+				$errors = arr::overwrite($errors, $_POST->errors('auth'));
+				$form_error = TRUE;
 
 			}
 		}
 
 		$this->template->content->errors = $errors;
-        $this->template->content->form = $form;
-        $this->template->content->form_error = $form_error;
-    }
+		$this->template->content->form = $form;
+		$this->template->content->form_error = $form_error;
+	}
 
-    public function manage()
-    {
-    	// if not logged in, go back to the start
-    	$session = Session::instance();
-    	$mhi_user_id = $session->get('mhi_user_id');
-        if($mhi_user_id == FALSE){
-        	url::redirect('/');
-        }
+	public function manage()
+	{
+		// If not logged in, go back to the start
 
-    	$this->template->header->this_page = 'mhi';
-        $this->template->content = new View('mhi_manage');
-
-        $this->template->content->domain_name = $_SERVER['HTTP_HOST'].Kohana::config('config.site_domain');
-
-        $mhi_site = new Mhi_Site_Model();
-        $this->template->content->sites = $mhi_site->get_user_sites($mhi_user_id);
-    }
-
-    public function account()
-    {
-		// if not logged in, go back to the start
 		$session = Session::instance();
 		$mhi_user_id = $session->get('mhi_user_id');
-		if($mhi_user_id == FALSE){
+
+		if ($mhi_user_id == FALSE)
+			url::redirect('/');
+
+		$this->template->header->this_page = 'mhi';
+		$this->template->content = new View('mhi_manage');
+
+		$this->template->content->domain_name = $_SERVER['HTTP_HOST'].Kohana::config('config.site_domain');
+
+		$mhi_site = new Mhi_Site_Model;
+		$this->template->content->sites = $mhi_site->get_user_sites($mhi_user_id);
+	}
+
+	public function account()
+	{
+		// If not logged in, go back to the start
+
+		$session = Session::instance();
+		$mhi_user_id = $session->get('mhi_user_id');
+		if ($mhi_user_id == FALSE)
+		{
 			url::redirect('/');
 		}
 
 		$this->template->header->this_page = 'mhi';
 		$this->template->content = new View('mhi_account');
 
-        $mhi_user = new Mhi_User_Model();
+		$mhi_user = new Mhi_User_Model;
 
-        // Get user info
+		// Get user info
+
 		$this->template->content->user = $mhi_user->get($mhi_user_id);
 
-    }
+	}
 
-    public function logout()
-    {
-    	$mhi_user = new Mhi_User_Model();
-    	$mhi_user->logout();
-    	url::redirect('/');
-    }
+	public function logout()
+	{
+		$mhi_user = new Mhi_User_Model;
+		$mhi_user->logout();
+		url::redirect('/');
+	}
 
-    public function signup()
-    {
-    	$this->template->header->this_page = 'mhi';
-        $this->template->content = new View('mhi_signup');
-        $this->template->content->site_name = Kohana::config('settings.site_name');
-        $this->template->content->domain_name = $_SERVER['HTTP_HOST'].Kohana::config('config.site_domain');
+	public function signup()
+	{
+		$this->template->header->this_page = 'mhi';
+		$this->template->content = new View('mhi_signup');
+		$this->template->content->site_name = Kohana::config('settings.site_name');
+		$this->template->content->domain_name = $_SERVER['HTTP_HOST'].Kohana::config('config.site_domain');
 
-        $session = Session::instance();
-        $this->template->content->logged_in = $session->get('mhi_user_id');
-    }
+		$session = Session::instance();
+		$this->template->content->logged_in = $session->get('mhi_user_id');
+	}
 
-    public function create()
-    {
-    	$this->template->header->this_page = 'mhi';
-        $this->template->content = new View('mhi_create');
+	public function create()
+	{
+		$this->template->header->this_page = 'mhi';
+		$this->template->content = new View('mhi_create');
 
-        // Process Form
-        if($_POST){
+		// Process Form
+
+		if ($_POST)
+		{
 			$post = Validation::factory($_POST);
 
-			//Trim whitespaces
+			// Trim whitespaces
+
 			$post->pre_filter('trim');
 
 			$session = Session::instance();
 			$mhi_user_id = $session->get('mhi_user_id');
 
-			//These rules are only required if we aren't already logged in
-			if($mhi_user_id == FALSE){
+			// These rules are only required if we aren't already logged in
+
+			if ($mhi_user_id == FALSE)
+			{
 				$post->add_rules('signup_first_name','required','alpha_dash');
 				$post->add_rules('signup_last_name','required','alpha_dash');
 				$post->add_rules('signup_email', 'required','email');
@@ -180,25 +198,28 @@ class MHI_Controller extends Template_Controller
 			$post->add_rules('signup_instance_name','required');
 			$post->add_rules('signup_instance_tagline','required');
 
-			if($post->validate()) {
+			if ($post->validate())
+			{
 
-				$mhi_user = new Mhi_User_Model();
-				$db_genesis = new DBGenesis();
-				$mhi_site_database = new Mhi_Site_Database_Model();
-				$mhi_site = new Mhi_Site_Model();
+				$mhi_user = new Mhi_User_Model;
+				$db_genesis = new DBGenesis;
+				$mhi_site_database = new Mhi_Site_Database_Model;
+				$mhi_site = new Mhi_Site_Model;
 
 				// Check passwords if logged in and create user if not
-				if($mhi_user_id != FALSE){
+
+				if ($mhi_user_id != FALSE)
+				{
 
 					// Get user info
+
 					$user = $mhi_user->get($mhi_user_id);
 
 					$salt = Kohana::config('auth.salt_pattern');
 					$verify_password = sha1($post->verify_password.$salt);
 
-					if($verify_password != $user->password) {
+					if ($verify_password != $user->password)
 						throw new Kohana_User_Exception('Password Match Error', "Passwords do not match. Dev TODO: Come back later and clean up validation!");
-					}
 
 					$user_id = $mhi_user_id;
 					$email = $user->email;
@@ -208,6 +229,7 @@ class MHI_Controller extends Template_Controller
 				}else{
 
 					// Save new user
+
 					$user_id = $mhi_user->save_user(array(
 						'firstname'=>$post->signup_first_name,
 						'lastname'=>$post->signup_last_name,
@@ -220,8 +242,6 @@ class MHI_Controller extends Template_Controller
 					$password = $post->signup_password;
 				}
 
-
-
 				// Set up DB and Site
 
 				$base_db = $db_genesis->current_db();
@@ -229,15 +249,20 @@ class MHI_Controller extends Template_Controller
 				$new_db_name = $base_db.'_'.$post->signup_subdomain;
 
 				// Do some not so graceful validation
-				if($mhi_site_database->db_assigned($new_db_name) || $db_genesis->db_exists($new_db_name)) throw new Kohana_User_Exception('MHI Site Setup Error', "Database already exists and/or is already assigned in the MHI DB.");
-				if($mhi_site->domain_exists($post->signup_subdomain)) throw new Kohana_User_Exception('MHI Site Setup Error', "Domain already assigned in MHI DB.");
+
+				if ($mhi_site_database->db_assigned($new_db_name) OR $db_genesis->db_exists($new_db_name))
+					throw new Kohana_User_Exception('MHI Site Setup Error', "Database already exists and/or is already assigned in the MHI DB.");
+
+				if ($mhi_site->domain_exists($post->signup_subdomain))
+					throw new Kohana_User_Exception('MHI Site Setup Error', "Domain already assigned in MHI DB.");
 
 				// Create site
+
 				$site_id = $mhi_site->save_site(array(
 					'user_id'=>$user_id,
 					'site_domain'=>$post->signup_subdomain,
-					'site_privacy'=>1,    // TODO: 1 is the hardcoded default for now. Needs to be changed?
-					'site_active'=>1      // TODO: 1 is the default. This needs to be a config item since this essentially "auto-approves" sites
+					'site_privacy'=>1,	// TODO: 1 is the hardcoded default for now. Needs to be changed?
+					'site_active'=>1    // TODO: 1 is the default. This needs to be a config item since this essentially "auto-approves" sites
 				));
 
 				// Set up database and save details to MHI DB
@@ -260,7 +285,8 @@ class MHI_Controller extends Template_Controller
 
 		}else{
 			// If the form was never posted, we need to complain about it.
+
 			throw new Kohana_User_Exception('Incomplete Form', "Form not posted.");
 		}
-    }
+	}
 }
