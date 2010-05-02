4cef49eab2563ae72405698abcca24f219d12e01
David Kobia <david@ushahidi.com>
2010-05-02 18:28:07 -0400

    * Fixed wrong link in json cluster markers * closed #598

--- application/controllers/json.php
+++ application/controllers/json.php
@@ -336,7 +336,7 @@ class Json_Controller extends Template_Controller
 			$json_item = "{";
 		    $json_item .= "\"type\":\"Feature\",";
 		    $json_item .= "\"properties\": {";
-			$json_item .= "\"name\":\"" . str_replace(chr(10), ' ', str_replace(chr(13), ' ', "<a href=" . url::base() . "reports/view/" . $marker['id'] . "/>1 Report</a>")) . "\",";	
+			$json_item .= "\"name\":\"" . str_replace(chr(10), ' ', str_replace(chr(13), ' ', "<a href=" . url::base() . "reports/view/" . $single['id'] . "/>1 Report</a>")) . "\",";	
 		    $json_item .= "\"category\":[0], ";
 			$json_item .= "\"color\": \"".$color."\", ";
 			$json_item .= "\"icon\": \"".$icon."\", ";
