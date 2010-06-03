804c54bcc944677ceefd9913933533f9680b21c2
Brian Herbert <brian@ushahidi.com>
2010-06-03 14:41:01 -0400

    Adding an MHI config file.

---
+++ application/config/mhi.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Array of subdomains you would like to prevent MHI users from using.
+ *
+ */
+$config['blocked_subdomains'] = array('test','api');
+
+?>
