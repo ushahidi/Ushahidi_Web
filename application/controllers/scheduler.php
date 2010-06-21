dae4bc189330a4cf3a03752ad42e5d066f439f43
David Kobia <david@ushahidi.com>
2010-06-21 11:28:24 -0400

    * Scheduler 1x1 image now renders properly across OS's

--- application/controllers/scheduler.php
+++ application/controllers/scheduler.php
@@ -151,8 +151,12 @@ class Scheduler_Controller extends Controller
 			Header( "Expires: Wed, 11 Nov 1998 11:11:11 GMT");
 			Header( "Cache-Control: no-cache");
 			Header( "Cache-Control: must-revalidate");
-
-			printf ("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%",71,73,70,56,57,97,1,0,1,0,128,255,0,192,192,192,0,0,0,33,249,4,1,0,0,0,0,44,0,0,0,0,1,0,1,0,0,2,2,68,1,0,59);
+			Header( "Content-Length: 49");
+			echo pack('H*',
+				 '47494638396101000100910000000000ffffffff'
+				.'ffff00000021f90405140002002c000000000100'
+				.'01000002025401003b'
+			);
 		}
 	}
 }
