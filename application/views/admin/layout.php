ea473c27d3e86a03f1ba8fe7662e8afffe3e9864
Brian Herbert <brian@ushahidi.com>
2010-08-03 14:23:46 -0400

    Removing version checker. Will reimplement as a plugin in version 2.

--- application/views/admin/layout.php
+++ application/views/admin/layout.php
@@ -115,13 +115,6 @@
 		<div id="header">
 			<!-- top-area -->
 			<div class="top">
-				<strong>
-				<?php echo ucfirst(Kohana::lang('ui_admin.code')); ?> <?php echo Kohana::lang('ui_admin.version'); ?> <?php echo $code_version; ?>
-				<?php if($code_version != $actual_code_version) echo '<span>'.Kohana::lang('ui_admin.code_out_of_sync').' v'.$actual_code_version.'</span>'; ?>
-				 - 
-				<?php echo ucfirst(Kohana::lang('ui_admin.database')); ?> <?php echo Kohana::lang('ui_admin.version'); ?> <?php echo $actual_db_version; ?>
-				<?php if($db_version != $actual_db_version) echo '<span>'.Kohana::lang('ui_admin.db_out_of_sync').' v'.$db_version.'</span>'; ?>
-				</strong>
 				<ul>
 					<li class="none-separator"> <?php echo Kohana::lang('ui_admin.welcome');echo $admin_name; ?>!</li>
 					<li class="none-separator"><a href="<?php echo url::site() ?>" title="View the home page">
