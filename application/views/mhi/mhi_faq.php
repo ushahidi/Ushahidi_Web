c9f115823c1a816fad310b89a144b7812bcfb33c
Brian Herbert <brian@ushahidi.com>
2010-08-03 14:03:33 -0400

    MHI: Added more to FAQ

--- application/views/mhi/mhi_faq.php
+++ application/views/mhi/mhi_faq.php
@@ -29,6 +29,15 @@
 						<li><a href="#5">Can I use my own domain name?</a></li>
 						<li><a href="#6">Can I advertise on my deployment?</a></li>
 						<li><a href="#7">Can I change my username?</a></li>
+						<li><a href="#8">What can kinds of things can I use my map for?</a></li>
+						<li><a href="#9">Will this work with Ushahidi plugins?</a></li>
+						<li><a href="#10">How can I track my stats?</a></li>
+						<li><a href="#11">I have a specific question that's not answered here, who can I contact for more information or support?</a></li>
+						<li><a href="#12">Will this be available in multiple languages?</a></li>
+						<li><a href="#13">Can I set up a private account with limited access i.e a non-public account?</a></li>
+						<li><a href="#14">Can I integrate Swift River into my account?</a></li>
+						<li><a href="#15">Is there a way to block spam reports?</a></li>
+						<li><a href="#16">Will it be possible for me to export my data?</a></li>
 					</ul>
                 </p>
                 
@@ -47,7 +56,7 @@
                 <p>
                 	<a name="3"></a>
                 	<h4>How does Crowdmap work with SMS reports?</h4>
-                	Your deployment will not automatically support SMS immediately after you create your deployment. To get this to work, you must create an account at <a href="www.clickatell.com">Clickatell</a> or set up <a href="FrontlineSMS">FrontlineSMS</a> to work with your deployment. To get it to work with your deployment, you will need to follow the instructions in your deployment. These can be found here: http://[YOURDEPLOYMENT].crowdmap.com/admin/settings/sms
+                	Your deployment will not automatically support SMS immediately after you create your deployment. To get this to work, you must create an account at <a href="www.clickatell.com">Clickatell</a> or set up <a href="FrontlineSMS">FrontlineSMS</a> to work with your deployment. To get it to work with your deployment, you will need to follow the instructions in the admin panel of your deployment. These instructions can be found here: http://[YOURDEPLOYMENT].crowdmap.com/admin/settings/sms<br/><br/>If you are looking for non-technical information regarding the approval and verification of SMS messages, check out, <a href="http://ushahidi.com/media/c_Ushahidi-Verification-Guide.pdf">"A Guide To Verification"</a> on the <a href="http://ushahidi.com/community_resources">Ushahidi Community Resources</a> page.
                 </p>
                 
                 <p>
@@ -72,6 +81,60 @@
                 	<a name="7"></a>
                 	<h4>How do I change my username, password or name?</h4>
                 	Log into your account and go to the <a href="http://crowdmap.com/mhi/account">Account Settings</a>. From here you are able to change your email address, which is also your username. You are also able to change your password and your name.
+                </p>
+                
+                <p>
+                	<a name="8"></a>
+                	<h4>What can kinds of things can I use my map for?</h4>
+                	To see what some other people and organizations have done with Ushahidi. We have curated a <a href="http://ushahidi.com/platform">list of deployments</a> on the Ushahidi website so you can get some ideas.
+                </p>
+                
+                <p>
+                	<a name="9"></a>
+                	<h4>Will this work with Ushahidi plugins?</h4>
+                	As of right now, Crowdmap does not offer support for plugins. However, this is something we are very excited about and will be providing approved plugins for your use in the future!
+                </p>
+                
+                <p>
+                	<a name="10"></a>
+                	<h4>How can I track my stats?</h4>
+                	You will be able to check statistics in the admin panel of your deployment. Just visit http://[YOURDEPLOYMENT].crowdmap.com/admin/stats to see these stats. If you would like to use a service like Google Analytics, you will be able to put the web property ID provided to you by Google Analytics on the settings page in the admin panel. This is located at http://[YOURDEPLOYMENT].crowdmap.com/admin/settings/site. Visit <a href="http://www.google.com/analytics/">Google Analytics</a> for more information.
+                </p>
+                
+                <p>
+                	<a name="11"></a>
+                	<h4>I have a specific question that's not answered here, who can I contact for more information or support?</h4>
+                	If you would like to get in touch with us, send us a message on the <a href="http://crowdmap.com/mhi/contact">Contact Us</a> page.
+                </p>
+                
+                <p>
+                	<a name="12"></a>
+                	<h4>Will this be available in multiple languages?</h4>
+                	xxx
+                </p>
+                
+                <p>
+                	<a name="13"></a>
+                	<h4>Can I set up a private account with limited access i.e a non-public account?</h4>
+                	Not at this time.
+                </p>
+                
+                <p>
+                	<a name="14"></a>
+                	<h4>Can I integrate Swift River into my account?</h4>
+                	xxx
+                </p>
+                
+                <p>
+                	<a name="15"></a>
+                	<h4>Is there a way to block spam reports?</h4>
+                	Not at this time. If you are having trouble with spammers, please get in touch with us on the <a href="http://crowdmap.com/mhi/contact">Contact Us</a> page.
+                </p>
+                
+                <p>
+                	<a name="16"></a>
+                	<h4>Will it be possible for me to export my data?</h4>
+                	In the admin panel, you can find the Download Reports options at http://[YOURDEPLOYMENT].crowdmap.com/admin/reports/download.
                 </p>
             
             </div></div>
