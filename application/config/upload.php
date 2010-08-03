93e4e5dce10f13bdddea1cad7e40adb96cb774b0
Brian Herbert <brian@ushahidi.com>
2010-08-03 18:26:30 -0400

    Relative dir should actually be the relative dir.

--- application/config/upload.php
+++ application/config/upload.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 $config['directory'] = DOCROOT.'media/uploads';
-$config['relative_directory'] = DOCROOT.'media/uploads';
+$config['relative_directory'] = 'media/uploads';
 $config['create_directories'] = TRUE;
 $conif['remove_spaces'] = TRUE;
 
