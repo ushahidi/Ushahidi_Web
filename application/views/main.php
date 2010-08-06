cc652323dfc6f18a770f4fb23b89288660483f95
Brian Herbert <brian@ushahidi.com>
2010-08-06 15:37:50 -0400

    Removed extraneous commented out HTML.

--- application/views/main.php
+++ application/views/main.php
@@ -260,18 +260,6 @@
 				
 					</div>
 					<!-- /content blocks -->
-<?php
-/*
- *					<!-- site footer -->
- *					<div class="site-footer">
- *
- *						<h5>Site Footer</h5>
- *						Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris porta. Sed eget nisi. Fusce rhoncus lorem ac erat. Maecenas turpis tellus, volutpat quis, sodales et, consectetuer ac, est. Nullam sed est sed augue vestibulum condimentum. In tellus. Integer luctus odio eu arcu. Pellentesque imperdiet felis eu tortor. Morbi ante dui, iaculis id, vulputate sit amet, venenatis in, turpis. Fusce in risus.
- *
- *					</div>
- *					<!-- / site footer -->
-*/
-?>
 			
 				</div>
 				<!-- content -->
