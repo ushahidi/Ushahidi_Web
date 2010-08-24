9e260c6fcf968a3737057b3a1dd305bd29f75d1d
David Kobia <david@ushahidi.com>
2010-08-24 17:05:16 -0400

    * Fixed issue with Dashboard and Incident Model * closed #871

--- application/models/incident.php
+++ application/models/incident.php
@@ -203,16 +203,20 @@ class Incident_Model extends ORM
 	*/
 	public static function get_number_reports_by_date($range=NULL)
 	{
-
+		$db = new Database();
+		
 		if ($range == NULL)
 		{
-			$query = 'SELECT COUNT(id) as count, DATE(incident_date) as date, MONTH(incident_date) as month, DAY(incident_date) as day, YEAR(incident_date) as year FROM incident GROUP BY date ORDER BY incident_date ASC';
+			$sql = 'SELECT COUNT(id) as count, DATE(incident_date) as date, MONTH(incident_date) as month, DAY(incident_date) as day, YEAR(incident_date) as year FROM incident GROUP BY date ORDER BY incident_date ASC';
 		}else{
-			$query = 'SELECT COUNT(id) as count, DATE(incident_date) as date, MONTH(incident_date) as month, DAY(incident_date) as day, YEAR(incident_date) as year FROM incident WHERE incident_date >= DATE_SUB(CURDATE(), INTERVAL '.mysql_escape_string($range).' DAY) GROUP BY date ORDER BY incident_date ASC';
+			$sql = 'SELECT COUNT(id) as count, DATE(incident_date) as date, MONTH(incident_date) as month, DAY(incident_date) as day, YEAR(incident_date) as year FROM incident WHERE incident_date >= DATE_SUB(CURDATE(), INTERVAL '.mysql_escape_string($range).' DAY) GROUP BY date ORDER BY incident_date ASC';
 		}
-		$result = mysql_query($query);
+		
+		$query = $db->query($sql);
+		$result = $query->result_array(FALSE);
+		
 		$array = array();
-		while ($row = mysql_fetch_assoc($result))
+		foreach ($result AS $row)
 		{
 			$timestamp = mktime(0,0,0,$row['month'],$row['day'],$row['year'])*1000;
 			$array["$timestamp"] = $row['count'];
