fb7d392edbb97566924fe697e84f8ea0d4d3b617
Brian Herbert <brian@ushahidi.com>
2010-08-26 11:28:11 -0400

    Fixed primary key issue.

--- sql/ushahidi.sql
+++ sql/ushahidi.sql
@@ -1505,7 +1505,7 @@ CREATE TABLE `mhi_log` (
 CREATE TABLE `mhi_log_actions` (
   `id` int(11) NOT NULL,
   `description` text NOT NULL,
-  PRIMARY KEY  (`int`)
+  PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 INSERT INTO `mhi_log_actions` (`id`, `description`) VALUES
