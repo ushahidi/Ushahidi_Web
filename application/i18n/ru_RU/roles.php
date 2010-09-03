a00e2c6823c6dc31e64726582c979d7c7e8be89b
thetoon <sergei.plaxienko+wordpress@gmail.com>
2010-09-03 09:17:50 +0700

    Missed translated files

---
+++ application/i18n/ru_RU/roles.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * ROLES INTERFACE INTERNATIONALIZATION
+ *
+ * Translated by Sergei 'the_toon' Plaxienko sergei.plaxienko@gmail.com
+ *
+ * ru_RU
+ *
+ */
+
+$lang = array
+(
+	'name' => array
+	(
+		'required'	=> 'Поле имени должно быть заполнено.',
+		'alpha_numeric'	=> 'Поле имени может содержать только буквы и цифры.',
+		'nomodify'      => 'Роль SuperAdmin не может быть изменена.',
+		'length'	=> 'Поле имени должно быть не меньше 2х и не больше 30 символов.',
+	),
+	
+	'description' => array
+	(
+		'required'	=> 'Поле описания должно быть заполнено.',
+		'length'	=> 'Поле описания должно быть не меньше 3х и не больше 100 символов.'
+	),
+);
+?>
