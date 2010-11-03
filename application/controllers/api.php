bb47369b90b5987c0556e782db69ed8f9d5cc18d
eyedol <addhen@linuxmail.org>
2010-11-03 01:13:00 +0000

    Updated API version number

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -2,7 +2,7 @@
 /**
  * This controller handles API requests.
  *
- * @version 26 - Henry Addo 2010-10-28
+ * @version 27 - Henry Addo 2010-11-03
  *
  * PHP version 5
  * LICENSE: This source file is subject to LGPL license
