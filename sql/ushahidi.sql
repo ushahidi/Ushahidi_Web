df478d64dfd6b9983cf72e84d44b64420d4ebb2b
Omar <ovruni@riseup.net>
2010-11-30 05:24:12 -0500

    Removing white space for table api_settings

--- sql/ushahidi.sql
+++ sql/ushahidi.sql
@@ -1332,13 +1332,13 @@ CREATE TABLE IF NOT EXISTS `layer` (                                            
 -- Definition of table `api_settings`
 --
 
-CREATE TABLE  IF NOT EXISTS `api_settings` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `default_record_limit` int(11) NOT NULL DEFAULT '20',
-  `max_record_limit` int(11) DEFAULT NULL,
-  `max_requests_per_ip_address` int(11) DEFAULT NULL,
-  `max_requests_quota_basis` int(11) DEFAULT NULL,
-  `modification_date` datetime NOT NULL,
+CREATE TABLE IF NOT EXISTS `api_settings` (                                         -- table description
+  `id` int(11) NOT NULL AUTO_INCREMENT,	                                            -- field description
+  `default_record_limit` int(11) NOT NULL DEFAULT '20',                             -- field description
+  `max_record_limit` int(11) DEFAULT NULL,                                          -- field description
+  `max_requests_per_ip_address` int(11) DEFAULT NULL,                               -- field description
+  `max_requests_quota_basis` int(11) DEFAULT NULL,                                  -- field description
+  `modification_date` datetime NOT NULL,                                            -- field description
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='For storing API logging settings';
 
