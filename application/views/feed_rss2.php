59dc751b83d26334400666769b20f5c69601432f
Reinier Battenberg <reinier.battenberg@mountbatten.net>
2011-02-13 16:47:53 +0300

    Adds RSS <category> support

--- application/views/feed_rss2.php
+++ application/views/feed_rss2.php
@@ -7,7 +7,7 @@
 		<description><?php echo $feed_description; ?></description>
 		<generator>Ushahidi Engine</generator>
 		<atom:link href="<?php echo $feed_url; ?>" rel="self" type="application/rss+xml" />
-		<?php
+<?php
 		// Event::feed_rss_head - Add to the feed head
 		Event::run('ushahidi_action.feed_rss_head');
 
@@ -19,12 +19,17 @@
 			<description><![CDATA[<?php echo $item['description']; ?>]]></description>
 			<pubDate><?php echo gmdate("D, d M Y H:i:s T", strtotime($item['date'])); ?></pubDate>
 			<guid><?php if(isset($item['guid'])) echo $item['guid']; else echo $item['link'] ?></guid>
-<?php if(isset($item['point'])) echo "\t\t\t<georss:point>".$item['point'][0]." ".$item['point'][1]."</georss:point>\n"; ?>
-			<?php
+<?php if(isset($item['point'])) echo "\t\t\t<georss:point>".$item['point'][0]." ".$item['point'][1]."</georss:point>\n";
+              foreach ($item['categories'] as $category)
+              {?>
+                        <category><?php echo $category; ?></category>
+<?php
+              }
 			// Event::feed_rss_item - Add to the feed item
 			Event::run('ushahidi_action.feed_rss_item', $item['id']);
 			?>
-		</item><?php 
+		</item>
+<?php 
 		}?>
 
 	</channel>
