0ac43fa06cdc5089989b1ddd1acc87c37470e74d
Brian Herbert <brianherbert@gmail.com>
2011-03-24 13:01:16 -0400

    Stripping tags, fixing bug #2164

--- application/views/admin/reports.php
+++ application/views/admin/reports.php
@@ -91,8 +91,8 @@
 								foreach ($incidents as $incident)
 								{
 									$incident_id = $incident->id;
-									$incident_title = $incident->incident_title;
-									$incident_description = text::limit_chars($incident->incident_description, 150, "...", true);
+									$incident_title = strip_tags($incident->incident_title);
+									$incident_description = text::limit_chars(strip_tags($incident->incident_description), 150, "...", true);
 									$incident_date = $incident->incident_date;
 									$incident_date = date('Y-m-d', strtotime($incident->incident_date));
 									$incident_mode = $incident->incident_mode;	// Mode of submission... WEB/SMS/EMAIL?
