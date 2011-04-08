f2e9ecba2b73a2d97c086793e48c862db2281103
Brian Herbert <brianherbert@gmail.com>
2011-04-08 16:27:46 -0400

    Removing filters.

--- themes/default/views/main.php
+++ themes/default/views/main.php
@@ -161,26 +161,16 @@
 		<!-- content column -->
 		<div id="content" class="clearingfix">
 			<div class="floatbox">
-			
+
 				<!-- filters -->
 				<div class="filters clearingfix">
-					<div style="float:left; width: 100%">
-						<strong><?php echo Kohana::lang('ui_main.filters'); ?></strong>
-						<ul>
-							<li><a id="media_0" class="active" href="#"><span><?php echo Kohana::lang('ui_main.reports'); ?></span></a></li>
-							<li><a id="media_4" href="#"><span><?php echo Kohana::lang('ui_main.news'); ?></span></a></li>
-							<li><a id="media_1" href="#"><span><?php echo Kohana::lang('ui_main.pictures'); ?></span></a></li>
-							<li><a id="media_2" href="#"><span><?php echo Kohana::lang('ui_main.video'); ?></span></a></li>
-							<li><a id="media_3" href="#"><span><?php echo Kohana::lang('ui_main.all'); ?></span></a></li>
-						</ul>
-					</div>
 					<?php
 					// Action::main_filters - Add items to the main_filters
 					Event::run('ushahidi_action.map_main_filters');
 					?>
 				</div>
 				<!-- / filters -->
-				
+
 				<?php								
 				// Map and Timeline Blocks
 				echo $div_map;
