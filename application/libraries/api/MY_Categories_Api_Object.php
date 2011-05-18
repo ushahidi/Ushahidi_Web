5bcc874bc351deab1da6e2859d105ac4dae71fba
John Etherton <john.etherton@gmail.com>
2011-05-17 18:03:43 -0600

    Closes #2190
    Depending on if you request all the categories or if you request a category by ID the json returns different keys to index the title, color, and description fields. This just confuses things and is unnecessary. I'm going to set the indexes to match the names used in the database:
    category_title
    category_description
    category_color

--- application/libraries/api/MY_Categories_Api_Object.php
+++ application/libraries/api/MY_Categories_Api_Object.php
@@ -140,8 +140,8 @@ class Categories_Api_Object extends Api_Object_Core {
         $ret_json_or_xml = ''; //will hold the json/xml string to return
 
         //find incidents
-        $this->query = "SELECT id, category_title AS title, category_description AS
-                description, category_color AS color FROM `".$this->table_prefix."category` WHERE
+        $this->query = "SELECT id, category_title, category_description, 
+        	category_color FROM `".$this->table_prefix."category` WHERE
                 category_visible = 1 ORDER BY id DESC";
         
         $items = $this->db->query($this->query);
