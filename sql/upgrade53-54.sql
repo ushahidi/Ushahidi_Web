9f6c7128cf2d4f003abb1209c5e5a1844d9ff07e
Emmanuel Kala <emmanuel@ushahidi.com>
2011-05-25 13:42:09 -0700

    * Modified upgrader script

--- sql/upgrade53-54.sql
+++ sql/upgrade53-54.sql
@@ -3,4 +3,4 @@ DROP TABLE IF EXISTS `organization_incident`;
 DROP TABLE IF EXISTS `organization`;
 
 -- Update the database version
-UPDATE `settings` SET `db_version` = 54 WHERE `id` = 1;
+UPDATE `settings` SET `db_version` = 54 WHERE `id` = 1 LIMIT 1;
