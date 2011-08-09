fe3d123db1242db8acf24de801283bb182c65404
Angela Oduor <angela@ushahidi.com>
2011-08-09 14:59:51 +0300

    Fixed a bug where clicking on edit would either not populate the fields at all, or would populate the fiels all wrong on the Admin Sharing Page

--- application/views/admin/sharing_js.php
+++ application/views/admin/sharing_js.php
@@ -16,7 +16,7 @@
  */
 ?>
 // Sharing JS
-function fillFields(id, sharing_url, sharing_color, sharing_limits, sharing_type)
+function fillFields(id, sharing_url, sharing_name, sharing_color)
 {
 	$("#sharing_id").attr("value", unescape(id));
 	$("#sharing_name").attr("value", unescape(sharing_name));
