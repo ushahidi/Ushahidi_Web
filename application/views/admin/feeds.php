cdd8a37b3bbcb3e87c82f4ab2419283aa30fe93c
Angela Oduor <angela@ushahidi.com>
2011-08-26 13:36:14 +0300

    Changed User prompts for Hiding/Showing Feeds

--- application/views/admin/feeds.php
+++ application/views/admin/feeds.php
@@ -104,7 +104,7 @@
 											<td class="col-4">
 												<ul>
 													<li class="none-separator"><a href="#add" onClick="fillFields('<?php echo(rawurlencode($feed_id)); ?>','<?php echo(rawurlencode($feed_name)); ?>','<?php echo(rawurlencode($feed_url)); ?>')"><?php echo Kohana::lang('ui_main.edit');?></a></li>
-													<li class="none-separator"><a href="javascript:feedAction('v','SHOW/HIDE','<?php echo(rawurlencode($feed_id)); ?>')"<?php if ($feed_active) echo " class=\"status_yes\"" ?>><?php echo Kohana::lang('ui_main.visible');?></a></li>
+													<li class="none-separator"><a class="status_yes" href="javascript:feedAction('v','SHOW/HIDE','<?php echo(rawurlencode($feed_id)); ?>')"><?php if ($feed_active) { echo Kohana::lang('ui_main.visible'); } else { echo Kohana::lang('ui_main.hidden'); } ?></a></li>
 													<li><a href="javascript:feedAction('d','DELETE','<?php echo(rawurlencode($feed_id)); ?>')" class="del"><?php echo Kohana::lang('ui_main.delete');?></a></li>
 												</ul>
 											</td>
