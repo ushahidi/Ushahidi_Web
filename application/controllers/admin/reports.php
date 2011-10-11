a969af3eb769c4b4cf2e24d23470f8c6981c8c9e
Brian Herbert <brianherbert@gmail.com>
2011-10-11 18:48:52 +0800

    Fixed CDN photos not being deleted from report edit in the admin.

--- application/controllers/admin/reports.php
+++ application/controllers/admin/reports.php
@@ -1326,17 +1326,28 @@ class Reports_Controller extends Admin_Controller {
 		{
 			$photo = ORM::factory('media', $id);
 			$photo_large = $photo->media_link;
+			$photo_medium = $photo->media_medium;
 			$photo_thumb = $photo->media_thumb;
-
-			// Delete Files from Directory
-			if ( ! empty($photo_large))
+			
+			if(file_exists(Kohana::config('upload.directory', TRUE).$photo_large))
 			{
-				unlink(Kohana::config('upload.directory', TRUE) . $photo_large);
+				unlink(Kohana::config('upload.directory', TRUE).$photo_large);
+			}elseif(Kohana::config("cdn.cdn_store_dynamic_content") AND valid::url($photo_large)){
+				cdn::delete($photo_large);
 			}
 			
-			if ( ! empty($photo_thumb))
+			if(file_exists(Kohana::config('upload.directory', TRUE).$photo_medium))
 			{
-				unlink(Kohana::config('upload.directory', TRUE) . $photo_thumb);
+				unlink(Kohana::config('upload.directory', TRUE).$photo_medium);
+			}elseif(Kohana::config("cdn.cdn_store_dynamic_content") AND valid::url($photo_medium)){
+				cdn::delete($photo_medium);
+			}
+
+			if(file_exists(Kohana::config('upload.directory', TRUE).$photo_thumb))
+			{
+				unlink(Kohana::config('upload.directory', TRUE).$photo_thumb);
+			}elseif(Kohana::config("cdn.cdn_store_dynamic_content") AND valid::url($photo_thumb)){
+				cdn::delete($photo_thumb);
 			}
 
 			// Finally Remove from DB
