ba2f1455779c4ee8477d9bb638556552aeb5e488
Brian Herbert <brianherbert@gmail.com>
2011-10-17 16:23:10 +0800

    Bumping up SQL version.

--- sql/upgrade67-68.sql
+++ sql/upgrade67-68.sql
@@ -3,4 +3,4 @@ ALTER TABLE `incident` DROP COLUMN `incident_source`;
 ALTER TABLE `incident` DROP COLUMN `incident_information`;
 
 -- Update the database version
-UPDATE `settings` SET db_version = '68' WHERE `id` = 1 LIMIT 1;
+UPDATE `settings` SET `db_version` = '68' WHERE `id` = 1 LIMIT 1;
