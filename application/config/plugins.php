08a065e703026d91ea378e574ed8bb8421a3c54a
Brian Herbert <brianherbert@gmail.com>
2011-10-17 16:46:32 +0800

    Add a "hide from list" config option to hide plugins from the plugin list. Closes Ushahidi_Web Redmine ticket number 2337.

---
+++ application/config/plugins.php
@@ -0,0 +1,14 @@
+<?php defined('SYSPATH') or die('No direct script access allowed.');
+/**
+ * Plugins Configuration
+ *
+ * This file contains configuration options for plugins
+ *
+ */
+
+// This setting will hide plugins from the plugin list in the admin panel.
+//   Must be an array of the dir names of the plugins you want to hide.
+//   i.e. 'smssync','viddler', etc...
+$config['hide_from_list'] = array();
+
+// TODO: Add other config options like "force enable", "force disable", etc.
