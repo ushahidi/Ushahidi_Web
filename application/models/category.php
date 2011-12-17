ae532b9ae65e17feaacad2d146d5465ed1542d43
Robbie Mackay <rm@robbiemackay.com>
2011-12-17 11:22:55 +0000

    Fix category order on reports submit page: add default ordering to category model

--- application/models/category.php
+++ application/models/category.php
@@ -33,6 +33,12 @@ class Category_Model extends ORM_Tree {
 	protected $children = "category";
 	
 	/**
+	 * Default sort order
+	 * @var array
+	 */
+	public $sorting = array("category_position" => "asc");
+	
+	/**
 	 * Validates and optionally saves a category record from an array
 	 *
 	 * @param array $array Values to check
