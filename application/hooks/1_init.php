4499e115e570b3f613424d9fbf3bfff0cf56c257
Evan Sims <evan@ushahidi.com>
2011-12-21 21:18:58 -0600

    Honor HTTPS when redirecting to the installer.

--- application/hooks/1_init.php
+++ application/hooks/1_init.php
@@ -7,14 +7,18 @@
 
 if (!file_exists(DOCROOT."application/config/database.php"))
 {
-	if ($_SERVER["SERVER_PORT"] != "80") {
+	if ($_SERVER["SERVER_PORT"] != "80" && $_SERVER["SERVER_PORT"] != "443") {
 		$url = $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
 	} else {
 		$url = $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
 	}
-		
-	$installer =  "http://$url"."installer/";
-		
+
+	$installer = "http://${url}installer/";
+
+	if (isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on') {
+		$installer = str_replace('http://', 'https://', $installer);
+	}
+
 	url::redirect($installer);
-	
+
 }
