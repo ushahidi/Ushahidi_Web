5a5a2a074c491aab1fdf9aa36ceefcf3b7d55e14
Tal Galili <Tal.Galili@gmail.com>
2012-02-20 20:04:16 +0200

    Creating a unicorn_RTL theme
    This is just a copy of unicorn, on the basis of which I can create a RTL version....

---
+++ themes/unicorn_RTL/readme.txt
@@ -0,0 +1,17 @@
+Theme Name: Unicorn
+Description: Map is front-and center with a simplified home page. Please note, this plugin requ
+Demo: http://www.ushahidi.com
+Version: 1.1
+Author: Caleb Bell
+Author Email: caleb@ushahidi.com
+
+
+===CHANGELOG===
+
+Unicorn v1.1, 09-19-2011
+---------------------------------
+* added "How to Report" toggle section to map on home page.
+
+Unicorn v1.0, 09-16-2011
+---------------------------------
+* version one released
---
+++ themes/unicorn_RTL/views/header.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+	<title><?php echo $site_name; ?></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700" rel="stylesheet" type="text/css">
+	<?php echo $header_block; ?>
+		<?php
+	// Action::header_scripts - Additional Inline Scripts from Plugins
+	Event::run('ushahidi_action.header_scripts');
+	?>
+</head>
+
+
+<?php
+  // Add a class to the body tag according to the page URI
+  // we're on the home page
+  if (count($uri_segments) == 0)
+  {
+    $body_class = "page-main";
+  }
+  // 1st tier pages
+  elseif (count($uri_segments) == 1)
+  {
+    $body_class = "page-".$uri_segments[0];
+  }
+  // 2nd tier pages... ie "/reports/submit"
+  elseif (count($uri_segments) >= 2)
+  {
+    $body_class = "page-".$uri_segments[0]."-".$uri_segments[1];
+  };
+
+?>
+
+<body id="page" class="<?php echo $body_class; ?>" />
+
+<?php echo $header_nav; ?>
+
+  <!-- top bar-->
+  <div id="top-bar">
+    <!-- searchbox -->
+		<div id="searchbox">
+
+			<!-- languages -->
+			<?php echo $languages;?>
+			<!-- / languages -->
+
+			<!-- searchform -->
+			<?php echo $search; ?>
+			<!-- / searchform -->
+
+    </div>
+  </div>
+  <!-- / searchbox -->
+
+
+	<!-- wrapper -->
+	<div class="rapidxwpr floatholder">
+
+		<!-- header -->
+		<div id="header">
+
+			<!-- logo -->
+			<?php if($banner == NULL){ ?>
+			<div id="logo">
+				<h1><a href="<?php echo url::site();?>"><?php echo $site_name; ?></a></h1>
+				<span><?php echo $site_tagline; ?></span>
+			</div>
+			<?php }else{ ?>
+			<a href="<?php echo url::site();?>"><img src="<?php echo $banner; ?>" alt="<?php echo $site_name; ?>" /></a>
+			<?php } ?>
+			<!-- / logo -->
+
+			<!-- submit incident -->
+			<?php echo $submit_btn; ?>
+			<!-- / submit incident -->
+
+		</div>
+		<!-- / header -->
+         <!-- / header item for plugins -->
+        <?php
+            // Action::header_item - Additional items to be added by plugins
+	        Event::run('ushahidi_action.header_item');
+        ?>
+		<!-- main body -->
+		<div id="middle">
+			<div class="background layoutleft">
+
+				<!-- mainmenu -->
+				<div id="mainmenu" class="clearingfix">
+					<ul>
+						<?php nav::main_tabs($this_page); ?>
+					</ul>
+
+				</div>
+				<!-- / mainmenu -->
