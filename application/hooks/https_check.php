c4ad8319a0566a3ba9ef16dab794ce69d35ce010
Brian Herbert <brianherbert@gmail.com>
2012-03-13 18:53:37 +0900

    Prevents non https sites from redirecting.

--- application/hooks/https_check.php
+++ application/hooks/https_check.php
@@ -19,38 +19,41 @@
 
 class https_check {
 
-    private $https_enabled;   // Flag to denote whether HTTPS is enabled/disabled
+	private $https_enabled;   // Flag to denote whether HTTPS is enabled/disabled
 
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        $this->https_enabled = Kohana::config('core.site_protocol');
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		$this->https_enabled = Kohana::config('core.site_protocol');
 
-        // Hook into routing
-        Event::add_after('system.routing', array('Router', 'setup'), array($this, 'rewrite_url'));
-    }
+		// Hook into routing
+		Event::add_after('system.routing', array('Router', 'setup'), array($this, 'rewrite_url'));
+	}
 
-    /**
-     * Rewrites the URL depending on whether HTTPS is enabled/disabled
-     *
-     * NOTES: - Emmanuel Kala, 18th Feb 2011
-     * This may bring issues with accessing the API (querying or posting) via mobile and/or external applications
-     * as they may not support querying information via HTTPS
-     *
-     */
-    public function rewrite_url()
-    {
-        $is_https_request = (array_key_exists('HTTPS', $_SERVER) AND $_SERVER['HTTPS'] == 'on')
-            ? TRUE
-            : FALSE;
+	/**
+	 * Rewrites the URL depending on whether HTTPS is enabled/disabled
+	 *
+	 * NOTES: - Emmanuel Kala, 18th Feb 2011
+	 * This may bring issues with accessing the API (querying or posting) via mobile and/or external applications
+	 * as they may not support querying information via HTTPS
+	 *
+	 */
+	public function rewrite_url()
+	{
+		if ($this->https_enabled == 'HTTPS')
+		{
+			$is_https_request = (array_key_exists('HTTPS', $_SERVER) AND $_SERVER['HTTPS'] == 'on')
+				? TRUE
+				: FALSE;
 
-        if (($this->https_enabled AND ! $is_https_request) OR ( ! $this->https_enabled AND $is_https_request))
-        {
-            url::redirect(url::base().url::current().Router::$query_string);
-        }
-    }
+			if (($this->https_enabled AND ! $is_https_request) OR ( ! $this->https_enabled AND $is_https_request))
+			{
+				url::redirect(url::base().url::current().Router::$query_string);
+			}
+		}
+	}
 }
 
 new https_check();
