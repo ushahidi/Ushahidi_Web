462cbb33942508f8850173f42a4416da0dda2797
Brian Herbert <brianherbert@gmail.com>
2012-03-13 16:56:54 +0900

    Update GA code to the latest version recommended by Google. Closes #319

--- application/libraries/Themes.php
+++ application/libraries/Themes.php
@@ -355,13 +355,18 @@ function runScheduler(img){img.onload = null;img.src = '".url::site().'scheduler
 		$html = "";
 		if (Kohana::config('settings.google_analytics') == TRUE) {
 			$html = "<script type=\"text/javascript\">
-				var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\");
-				document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));
-				</script>
-				<script type=\"text/javascript\">
-				var pageTracker = _gat._getTracker(\"" . Kohana::config('settings.google_analytics') . "\");
-				pageTracker._trackPageview();
-				</script>";
+
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', '".Kohana::config('settings.google_analytics')."']);
+			_gaq.push(['_trackPageview']);
+
+			(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+
+			</script>";
 		}
 		return $html;
 	}
