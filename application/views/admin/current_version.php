69fa95fecb118a643b376e046994c5bcb0a56b60
Robbie Mackay <rm@robbiemackay.com>
2012-04-20 09:15:54 +1200

    Fix version sync warning settings: Wrap both warnings, and seperate from auto_upgrader messages

--- application/views/admin/current_version.php
+++ application/views/admin/current_version.php
@@ -21,21 +21,26 @@
         <div id="update-info">
         <?php echo Kohana::lang('ui_admin.ushahidi');?> <?php echo $version; ?>
             <?php echo Kohana::lang('ui_admin.version_available');?>
-			        <a href="<?php echo url::site() ?>admin/upgrade" title="upgrade ushahidi"><?php echo Kohana::lang('ui_admin.update_link');?></a>
-        </div>
-    <?php } ?>
-
-    <?php if ( Kohana::config('config.enable_ver_sync_warning') == TRUE && ( url::current() != "admin/upgrade") && (Kohana::config('version.ushahidi_version') != Kohana::config('settings.ushahidi_version'))) { ?>
-        <div id="update-info">
-          <?php echo Kohana::lang('upgrade.upgrade_warning_software_version'); ?><br />
-          version.php: <?php echo Kohana::config('version.ushahidi_version') ?> &nbsp; <?php echo Kohana::lang('upgrade.upgrade_database'); ?>  <?php echo Kohana::config('settings.ushahidi_version') ?>
-        </div>
-    <?php } ?>
-
-    <?php if (( url::current() != "admin/upgrade") && (Kohana::config('version.ushahidi_db_version') != Kohana::config('settings.db_version'))) { ?>
-        <div id="update-info">
-          <?php echo Kohana::lang('upgrade.upgrade_warning_db_version'); ?><br />
-          version.php: <?php echo Kohana::config('version.ushahidi_db_version') ?> &nbsp; <?php echo Kohana::lang('upgrade.upgrade_database'); ?>  <?php echo Kohana::config('settings.db_version') ?>
+            <a href="<?php echo url::site() ?>admin/upgrade" title="upgrade ushahidi"><?php echo Kohana::lang('ui_admin.update_link');?></a>
         </div>
     <?php } ?>
 <?php }?>
+
+<?php 
+if ( Kohana::config('config.enable_ver_sync_warning') == TRUE) {
+  if ( ( url::current() != "admin/upgrade") && (Kohana::config('version.ushahidi_version') != Kohana::config('settings.ushahidi_version'))) { ?>
+    <div id="update-info">
+      <?php echo Kohana::lang('upgrade.upgrade_warning_software_version'); ?><br />
+      version.php: <?php echo Kohana::config('version.ushahidi_version') ?> &nbsp; <?php echo Kohana::lang('upgrade.upgrade_database'); ?>  <?php echo Kohana::config('settings.ushahidi_version') ?>
+    </div>
+  <?php
+  }
+  if (( url::current() != "admin/upgrade") && (Kohana::config('version.ushahidi_db_version') != Kohana::config('settings.db_version'))) { ?>
+    <div id="update-info">
+      <?php echo Kohana::lang('upgrade.upgrade_warning_db_version'); ?><br />
+      version.php: <?php echo Kohana::config('version.ushahidi_db_version') ?> &nbsp; <?php echo Kohana::lang('upgrade.upgrade_database'); ?>  <?php echo Kohana::config('settings.db_version') ?>
+    </div>
+<?php
+  }
+}
+?>
