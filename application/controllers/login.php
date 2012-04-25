aedd38ecc0a94baece4ca17eab67c3190864c5e1
Robbie Mackay <rm@robbiemackay.com>
2012-04-25 14:56:36 +1200

    Fix previous login change #441

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -60,7 +60,7 @@ class Login_Controller extends Template_Controller {
 
 				// Admins go to the admin panel
 				// Temporary fix - any non member role gets admin access
-				if($this->auth->logged_in('login') AND ! $this->auth->logged_in('member'))
+				if($auth->logged_in('login') AND ! $auth->logged_in('member'))
 				{
 					url::redirect('admin');
 				}
