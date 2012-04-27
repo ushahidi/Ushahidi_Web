0df105747bcdea3fe285cfb202172ab0196d5af1
Robbie Mackay <rm@robbiemackay.com>
2012-04-27 15:05:22 +1200

    Fix insecure cookie+session issues: default to session in DB, Add encryption config. #451

--- application/config/session.php
+++ application/config/session.php
@@ -1,5 +1,46 @@
 <?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * @package Session
+ *
+ * Session driver name.
+ * WARNING : Do not use 'cookie' driver, potential security issue
+ */
+$config['driver'] = 'database';
 
-// Enable session encryption
+/**
+ * Session storage parameter, used by drivers.
+ */
+//$config['storage'] = '';
+
+/**
+ * Session name.
+ * It must contain only alphanumeric characters and underscores. At least one letter must be present.
+ */
 $config['name'] = 'ushahidi';
+
+/**
+ * Session parameters to validate: user_agent, ip_address, expiration.
+ */
+$config['validate'] = array('user_agent', 'ip_address');
+
+/**
+ * Enable or disable session encryption.
+ */
 $config['encryption'] = TRUE;
+
+/**
+ * Session lifetime. Number of seconds that each session will last.
+ * A value of 0 will keep the session active until the browser is closed (with a limit of 24h).
+ */
+//$config['expiration'] = 7200;
+
+/**
+ * Number of page loads before the session id is regenerated.
+ * A value of 0 will disable automatic session id regeneration.
+ */
+//$config['regenerate'] = 3;
+
+/**
+ * Percentage probability that the gc (garbage collection) routine is started.
+ */
+//$config['gc_probability'] = 2;
