ae30be67d7f177ab9ff696efa73451a774ba979a
Yauhen Kharuzhy <jekhor@gmail.com>
2012-04-29 21:17:30 +0300

    Retrieve cities' names in site language
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

--- application/controllers/admin/settings.php
+++ application/controllers/admin/settings.php
@@ -1045,6 +1045,7 @@ class Settings_Controller extends Admin_Controller
 		$this->template = "";
 		$this->auto_render = FALSE;
 
+		$settings = ORM::factory('settings', 1);
 		$cities = 0;
 
 		// Get country ISO code from DB
@@ -1054,9 +1055,10 @@ class Settings_Controller extends Admin_Controller
 		{
 			$iso = $country->iso;
 
+			$lang = substr($settings->site_language, 0, 2);
 			// GeoNames WebService URL + Country ISO Code
 			$geonames_url = "http://ws.geonames.org/search?country="
-							.$iso."&featureCode=PPL&featureCode=PPLA&featureCode=PPLC&maxRows=1000";
+							.$iso."&featureCode=PPL&featureCode=PPLA&featureCode=PPLC&maxRows=1000&lang=".$lang;
 
 			// Grabbing GeoNames requires cURL so we will check for that here.
 			if (!function_exists('curl_exec'))
