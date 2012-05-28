d8925af54aecc94e0417b5f6fc440804c98af2f6
s.rutto <sharonrutto@gmail.com>
2012-05-28 12:20:28 +0300

    Update application/views/admin/reports_upload_success.php

--- application/views/admin/reports_upload_success.php
+++ application/views/admin/reports_upload_success.php
@@ -18,7 +18,7 @@
 	<h2><?php print $title; ?> <span></span><a href="<?php print url::site() ?>admin/reports/download"><?php echo Kohana::lang('ui_main.download_reports');?></a><a href="<?php print url::site() ?>admin/reports"><?php echo Kohana::lang('ui_main.view_reports');?></a><a href="<?php print url::site() ?>admin/reports/edit"><?php echo Kohana::lang('ui_main.create_report');?></a></h2>
 	
 	<h3><?php echo Kohana::lang('ui_main.upload_successful');?></h3>
-	   <p><?php echo Kohana::lang('ui_main.succesfully_imported');?> <?php echo $imported; ?> of <?php echo $rowcount; ?> <?php echo Kohana::lang('ui_main.reports');?>.</p>
+	   <p><?php echo Kohana::lang('ui_main.successfully_imported');?> <?php echo $imported; ?> of <?php echo $rowcount; ?> <?php echo Kohana::lang('ui_main.reports');?>.</p>
 
 	
 	<?php if(count($notices)){  ?>  
