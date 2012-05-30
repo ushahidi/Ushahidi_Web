b16aee9ead413e25af62c8aada1c1162ed1c1296
Robbie Mackay <rm@robbiemackay.com>
2012-05-31 10:26:57 +1200

    Updating version.php

--- application/config/version.php
+++ application/config/version.php
@@ -3,11 +3,11 @@
  * The Ushahidi Engine version
  * Make sure to update the ushahidi_version in the settings table too!
  */
-$config['ushahidi_version'] = "2.3.2";
+$config['ushahidi_version'] = "2.4";
 
 
 /**
  * The Ushahidi Engine DB revision number
  * Increments when changes are made to the Ushahidi DB schema.
  */
-$config['ushahidi_db_version'] = "89";
+$config['ushahidi_db_version'] = "90";
