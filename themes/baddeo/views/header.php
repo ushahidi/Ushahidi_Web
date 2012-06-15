575285ac1d1fbea52c8589446d534df49da0b544
matteomenapace <iosonomatteo@gmail.com>
2012-06-15 19:41:26 +0100

    Created a new theme

---
+++ themes/baddeo/views/header.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+	<title><?php echo $page_title.$site_name; ?></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700" rel="stylesheet" type="text/css">
+	<?php echo $header_block; ?>
+		<?php
+	// Action::header_scripts - Additional Inline Scripts from Plugins
+	Event::run('ushahidi_action.header_scripts');
+	?>
+</head>
+
+
+<?php
+  // Add a class to the body tag according to the page URI
+  // we're on the home page
+  if (count($uri_segments) == 0)
+  {
+    $body_class = "page-main";
+  }
+  // 1st tier pages
+  elseif (count($uri_segments) == 1)
+  {
+    $body_class = "page-".$uri_segments[0];
+  }
+  // 2nd tier pages... ie "/reports/submit"
+  elseif (count($uri_segments) >= 2)
+  {
+    $body_class = "page-".$uri_segments[0]."-".$uri_segments[1];
+  }
+
+?>
+
+<body id="page" class="<?php echo $body_class; ?>">
+
+<?php echo $header_nav; ?>
+
+	<!-- wrapper -->
+	<div class="rapidxwpr floatholder">
+
+		<!-- header -->
+		<div id="header">
+
+			<!-- logo -->
+			<?php if ($banner == NULL): ?>
+			<div id="logo">
+				<h1><a href="<?php echo url::site();?>"><?php echo $site_name; ?></a></h1>
+				<span><?php echo $site_tagline; ?></span>
+			</div>
+			<?php else: ?>
+			<a href="<?php echo url::site();?>"><img src="<?php echo $banner; ?>" alt="<?php echo $site_name; ?>" /></a>
+			<?php endif; ?>
+			<!-- / logo -->
+
+			<!-- submit incident -->
+			<?php echo $submit_btn; ?>
+			<!-- / submit incident -->
+
+			<?php
+				// Action::main_sidebar - Add Items to the Entry Page Sidebar
+				Event::run('ushahidi_action.main_sidebar');
+			?>
+			
+			<?php if(isset($site_message) AND $site_message != '') { ?>
+				<div class="site-message">
+					<?php echo $site_message; ?>
+				</div>
+			<?php } ?>
+			
+			<div class="clear"></div>
+
+		</div>
+		<!-- / header -->
+         <!-- / header item for plugins -->
+        <?php
+            // Action::header_item - Additional items to be added by plugins
+	        Event::run('ushahidi_action.header_item');
+        ?>
+
+        
+
+		<!-- main body -->
+		<div id="middle">
+			<div class="background layoutleft">
+
+				<!-- mainmenu -->
+				<div id="mainmenu" class="clearingfix">
+					<ul>
+						<?php nav::main_tabs($this_page); ?>
+					</ul>
+					
+					<div id="searchbox">
+					
+						<!-- languages -->
+						<?php echo $languages;?>
+						<!-- / languages -->
+			
+						<!-- searchform -->
+						<?php echo $search; ?>
+						<!-- / searchform -->
+			
+				    </div>
+
+				</div>
+				<!-- / mainmenu -->
