fb8b4d7e76572dadc3e086064108e17c5fab9b2e
Robbie Mackay <rm@robbiemackay.com>
2012-06-22 10:19:31 +1200

    Move custom ushahidi extensions to url helper out of system into application #338

--- system/helpers/url.php
+++ system/helpers/url.php
@@ -81,49 +81,6 @@ class url_Core {
 	}
 
 	/**
-	 * Returns the base URL of the CDN or the standard base URL if no CDN is configured.
-	 *
-	 * @param   boolean  type of cdn url. Acceptable values are img, css, js
-	 * @return  string
-	 */
-	public static function file_loc($type)
-	{
-		// Pull CDN URL from the cdn settings if it's set. Otherwise use the base URL by default
-		switch($type){
-			case 'img':
-				$url = (Kohana::config("cdn.cdn_img")) ? Kohana::config("cdn.cdn_img") : url::base();
-				break;
-			case 'css':
-				$url = (Kohana::config("cdn.cdn_css")) ? Kohana::config("cdn.cdn_css") : url::base();
-				break;
-			case 'js':
-				$url = (Kohana::config("cdn.cdn_js")) ? Kohana::config("cdn.cdn_js") : url::base();
-				break;
-			default:
-				$url = url::base();
-		}
-
-		// Force a slash on the end of the URL
-		return rtrim($url, '/').'/';
-	}
-
-	/**
-	 * Converts a file location to an absolute URL or returns the absolute URL if absolute URL
-	 * is passed. This function is for uploaded files since it uses the configured upload dir
-	 *
-	 * @param   string  file location or full URL
-	 * @return  string
-	 */
-	public static function convert_uploaded_to_abs($file)
-	{
-		if(valid::url($file) == true){
-			return $file;
-		}
-
-		return url::base().Kohana::config('upload.relative_directory').'/'.$file;
-	}
-
-	/**
 	 * Fetches an absolute site URL based on a URI segment.
 	 *
 	 * @param   string  site URI to convert
