e47a1fcd28bcf66aabcf5fba171f58c24e200ca6
Angela Oduor <angela@ushahidi.com>
2012-07-06 11:07:09 +0300

    Closes #658 . File referencing error fixed.

--- plugins/smssync/controllers/admin/smssync_settings.php
+++ plugins/smssync/controllers/admin/smssync_settings.php
@@ -21,7 +21,7 @@ class Smssync_Settings_Controller extends Admin_Controller
 		$this->template->this_page = 'addons';
 		
 		// Standard Settings View
-		$this->template->content = new View("admin/addons/plugins_settings");
+		$this->template->content = new View("admin/addons/plugin_settings");
 		$this->template->content->title = "SMSSync Settings";
 		
 		// Settings Form View
