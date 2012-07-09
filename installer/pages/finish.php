547dae38ed6cb1ecec8875fd76521be4b32a9609
Robbie Mackay <rm@robbiemackay.com>
2012-07-09 18:36:45 +1200

    Fix indenting on installer finish page .. done with #684

--- installer/pages/finish.php
+++ installer/pages/finish.php
@@ -1,7 +1,7 @@
 <body>
 	<div id="ushahidi_install_container" class="advanced">
-	    <div id="ushahidi_login_logo"><img src="../media/img/admin/logo_login.gif" /></div>
-	    <div id="ushahidi_login">
+		<div id="ushahidi_login_logo"><img src="../media/img/admin/logo_login.gif" /></div>
+		<div id="ushahidi_login">
 			<ol class="progress-meter clearfix">
 				<li><span>Database</span></li>
 				<li><span>General</span></li>
@@ -12,12 +12,12 @@
 				<li><span>Admin Password</span></li>
 				<li class="active last"><span>Finished</span></li>
 			</ol>
-	       	<form method="POST" name="frm_install" style="line-height: 100%; margin-top: 0; margin-bottom: 0;">
+			<form method="POST" name="frm_install" style="line-height: 100%; margin-top: 0; margin-bottom: 0;">
 				<div class="feedback success">
-	             	<h2>Installation Successful!</h2>
+					<h2>Installation Successful!</h2>
 				</div>
-	            <p>To login, go to <a href="<?php echo $base_url; ?>admin" target="_blank">
-					<?php echo $base_url."admin"; ?></a> and use the following credentials:<br /><br />
+				<p>To login, go to <a href="<?php echo $base_url; ?>admin" target="_blank">
+				<?php echo $base_url."admin"; ?></a> and use the following credentials:<br /><br />
 				<strong>Login Email:</strong> <?php echo $admin_email; ?><br />
 				<strong>Password:</strong> (not shown)</p>
 				<p><strong>Other next steps...</strong></p>
