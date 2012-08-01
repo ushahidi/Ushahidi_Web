d2d239b75d95b6dd9bb2efebd81925bf37106dd1
Robbie Mackay <rm@robbiemackay.com>
2012-08-01 17:01:47 +1200

    Change TRUE to lowercase to avoid js error
    Booleans are case sensitive in javascript

--- application/libraries/Themes.php
+++ application/libraries/Themes.php
@@ -348,7 +348,7 @@ function runScheduler(img){img.onload = null;img.src = '".url::site().'scheduler
 			_gaq.push(['_trackPageview']);
 
 			(function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = TRUE;
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 			})();
