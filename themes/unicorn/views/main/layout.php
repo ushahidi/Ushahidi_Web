afc201e4aa54c84e79b2bd50125b2758eeb05d17
Robbie Mackay <rm@robbiemackay.com>
2012-10-02 13:49:53 +1300

    Update unicorn categories listing code copy/paste from default theme #858
    This includes html entity encoding and category descriptions on hover

--- themes/unicorn/views/main/layout.php
+++ themes/unicorn/views/main/layout.php
@@ -37,59 +37,89 @@ $(function(){
 					<h3><?php echo Kohana::lang('ui_main.category');?></h3>
 			
 					<ul id="category_switch" class="category-filters">
-						<?php
-						$color_css = 'class="swatch" style="background-color:#'.$default_map_all.'"';
-						$all_cat_image = '';
-						if($default_map_all_icon != NULL) {
-							$all_cat_image = html::image(array(
-								'src'=>$default_map_all_icon,
-								'style'=>'float:left;padding-right:5px;'
-							));
-							$color_css = '';
-						}
-						?>
-						<li><a class="active" id="cat_0" href="#"><span <?php echo $color_css;?>><?php echo $all_cat_image; ?></span><span class="category-title"><?php echo Kohana::lang('ui_main.all_categories');?></span></a></li>
-						<?php
-							foreach ($categories as $category => $category_info)
+					<?php
+					$color_css = 'class="swatch" style="background-color:#'.$default_map_all.'"';
+					$all_cat_image = '';
+					if ($default_map_all_icon != NULL)
+					{
+						$all_cat_image = html::image(array(
+							'src'=>$default_map_all_icon,
+							'style'=>'float:left;padding-right:5px;'
+						));
+						$color_css = '';
+					}
+					?>
+					<li>
+						<a class="active" id="cat_0" href="#">
+							<span <?php echo $color_css; ?>><?php echo $all_cat_image; ?></span>
+							<span class="category-title"><?php echo Kohana::lang('ui_main.all_categories');?></span>
+						</a>
+					</li>
+					<?php
+						foreach ($categories as $category => $category_info)
+						{
+							$category_title = htmlentities($category_info[0], ENT_QUOTES, "UTF-8");
+							$category_color = $category_info[1];
+							$category_image = ($category_info[2] != NULL)
+							    ? url::convert_uploaded_to_abs($category_info[2])
+							    : NULL;
+							$category_description = htmlentities(Category_Lang_Model::category_description($category), ENT_QUOTES, "UTF-8");
+	
+							$color_css = 'class="swatch" style="background-color:#'.$category_color.'"';
+							if ($category_info[2] != NULL)
 							{
-								$category_title = $category_info[0];
-								$category_color = $category_info[1];
-								$category_image = '';
-								$color_css = 'class="swatch" style="background-color:#'.$category_color.'"';
-								if($category_info[2] != NULL) {
-									$category_image = html::image(array(
-										'src'=>$category_info[2],
-										'style'=>'float:left;padding-right:5px;'
-										));
-									$color_css = '';
-								}
-								echo '<li><a href="#" id="cat_'. $category .'"><span '.$color_css.'>'.$category_image.'</span><span class="category-title">'.$category_title.'</span></a>';
-								// Get Children
-								echo '<div class="hide" id="child_'. $category .'">';
-								if( sizeof($category_info[3]) != 0)
-								{
-									echo '<ul>';
-									foreach ($category_info[3] as $child => $child_info)
-									{
-										$child_title = $child_info[0];
-										$child_color = $child_info[1];
-										$child_image = '';
-										$color_css = 'class="swatch" style="background-color:#'.$child_color.'"';
-										if($child_info[2] != NULL) {
-											$child_image = html::image(array(
-															'src'=>$child_info[2],
-															'style'=>'float:left;padding-right:5px;'
-															));
-											$color_css = '';
-										}
-										echo '<li style="padding-left:10px;"><a href="#" id="cat_'. $child .'"><span '.$color_css.'>'.$child_image.'</span><span class="category-title">'.$child_title.'</span></a></li>';
-									}
-									echo '</ul>';
-								}
-								echo '</div></li>';
+								$category_image = html::image(array(
+									'src'=>$category_image,
+									'style'=>'float:left;padding-right:5px;'
+									));
+								$color_css = '';
 							}
-						?>
+	
+							echo '<li>'
+							    . '<a href="#" id="cat_'. $category .'" title="'.$category_description.'">'
+							    . '<span '.$color_css.'>'.$category_image.'</span>'
+							    . '<span class="category-title">'.$category_title.'</span>'
+							    . '</a>';
+	
+							// Get Children
+							echo '<div class="hide" id="child_'. $category .'">';
+							if (sizeof($category_info[3]) != 0)
+							{
+								echo '<ul>';
+								foreach ($category_info[3] as $child => $child_info)
+								{
+									$child_title = htmlentities($child_info[0], ENT_QUOTES, "UTF-8");
+									$child_color = $child_info[1];
+									$child_image = ($child_info[2] != NULL)
+									    ? url::convert_uploaded_to_abs($child_info[2])
+									    : NULL;
+									$child_description = htmlentities(Category_Lang_Model::category_description($child), ENT_QUOTES, "UTF-8");
+									
+									$color_css = 'class="swatch" style="background-color:#'.$child_color.'"';
+									if ($child_info[2] != NULL)
+									{
+										$child_image = html::image(array(
+											'src' => $child_image,
+											'style' => 'float:left;padding-right:5px;'
+										));
+	
+										$color_css = '';
+									}
+	
+									echo '<li style="padding-left:20px;">'
+									    . '<a href="#" id="cat_'. $child .'" title="'.$child_description.'">'
+									    . '<span '.$color_css.'>'.$child_image.'</span>'
+									    . '<span class="category-title">'.$child_title.'</span>'
+									    . '</a>'
+									    . '</li>';
+								}
+								echo '</ul>';
+							}
+							echo '</div></li>';
+						}
+					?>
 					</ul>
+					<!-- / category filters -->
 
 				</div>
 				<!-- / report category filters -->
