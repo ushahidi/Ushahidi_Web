baac1343cd47f5232e2f70eb3884223627b096f4
Robbie Mackay <rm@robbiemackay.com>
2012-10-14 17:48:42 -0500

    Fix upgrade 55-56 #886

--- sql/upgrade55-56.sql
+++ sql/upgrade55-56.sql
@@ -1,3 +1,3 @@
-UPDATE `settings` SET `blocks` = replace(`blocks`, ";", "|");
+# Removed update - replacing semi-colon with pipe, it broke the installer
 
 UPDATE `settings` SET `db_version` = 56 WHERE `id` = 1 LIMIT 1;
