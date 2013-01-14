6df0b9a761f51c4f0b521893dee6bb1644c0abfa
Henry Addo <addhen@linuxmail.org>
2013-01-14 09:04:44 +0000

    Fix to using http auth for authentication when a deployment is private mode

--- application/libraries/MY_Auth.php
+++ application/libraries/MY_Auth.php
@@ -41,4 +41,60 @@ class Auth extends Auth_Core {
 		return $this->has_permission('admin_ui', $user);
 	}
 
+	/**
+     * Attempt to log user in via HTTP BASIC AUTH
+     *
+     * @return void
+     */
+    public function http_auth_login() {
+       //Get username and password
+		if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW']))
+		{
+		    $username = filter_var($_SERVER['PHP_AUTH_USER'],
+			    FILTER_SANITIZE_STRING,
+			    FILTER_FLAG_ENCODE_HIGH|FILTER_FLAG_ENCODE_LOW);
+ 
+			$password = filter_var($_SERVER['PHP_AUTH_PW'],
+			    FILTER_SANITIZE_STRING,
+			    FILTER_FLAG_ENCODE_HIGH|FILTER_FLAG_ENCODE_LOW);
+ 
+			$email = FALSE;
+			if(kohana::config('riverid.enable') == TRUE && filter_var($username, FILTER_VALIDATE_EMAIL))
+			{
+			    $email = $username;
+			}
+ 
+			try
+			{
+			    if (!$this->login($username, $password, FALSE, $email))
+				{
+                    $this->prompt_login();
+				}
+			}
+			catch (Exception $e)
+			{
+			    $this->prompt_login();
+			}
+ 
+		}
+ 
+		//prompt user to login
+		$this->prompt_login();
+
+    }
+
+	/**
+     * Prompts user to login.
+     *
+     * @param int user_id - The currently logged in user id to be passed as the
+     *                      realm value.
+     * @return void
+     */
+    public function prompt_login($user_id = 0)
+    {
+        header('WWW-Authenticate: Basic realm="'.$user_id.'"');
+        header('HTTP/1.0 401 Unauthorized');
+        die(sprintf("%s, %s",Kohana::lang('auth.username.required'),Kohana::lang('auth.password.required')));
+    }
+
 }
