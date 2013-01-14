f07a06b9f68c5fcc85e956669a2b54faaf39f2c9
Henry Addo <addhen@linuxmail.org>
2013-01-14 09:04:44 +0000

    Allow using http auth to login when a deployment is private mode
    * This makes all request allow login via http auth
    * Adds Auth::http_auth_login() and Auth::http_auth_prompt_login() to Auth library

--- application/libraries/MY_Controller.php
+++ application/libraries/MY_Controller.php
@@ -47,9 +47,12 @@ abstract class Controller extends Controller_Core {
 		// Are we logged in? if not, do we have an auto-login cookie?
 		if (! $this->auth->logged_in()) {
 			$this->auth->auto_login();
+
+			// Login user in via HTTP AUTH
+			$this->auth->http_auth_login();
 		}
 		
-		// Chceck private deployment access
+		// Check private deployment access
 		$controller_whitelist = array(
 			'login',
 			'riverid',
@@ -60,6 +63,7 @@ abstract class Controller extends Controller_Core {
 		{
 			if (!$this->auth->logged_in('login') AND ! in_array(Router::$controller, $controller_whitelist))
 			{
+				// Redirect to login form
 				url::redirect('login');
 			}
 		}
