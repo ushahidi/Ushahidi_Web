c59820ec81f6cb1769be831f1604b18b34e039b5
Robbie Mackay <rm@robbiemackay.com>
2013-01-23 10:28:18 +0300

    Fix missing table prefix when listing messages by reporter #992

--- application/controllers/admin/messages.php
+++ application/controllers/admin/messages.php
@@ -78,7 +78,7 @@ class Messages_Controller extends Admin_Controller {
 		// Do we have a reporter ID?
 		if (isset($_GET['rid']) AND !empty($_GET['rid']))
 		{
-			$filter .= ' AND message.reporter_id=\''.intval($_GET['rid']).'\'';
+			$filter .= ' AND '.$table_prefix.'message.reporter_id=\''.intval($_GET['rid']).'\'';
 		}
         
 		// ALL / Trusted / Spam
