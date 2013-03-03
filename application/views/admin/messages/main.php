c8f3ee8a5541525318ae4c19c58109ba637bdeb8
Robbie Mackay <rm@robbiemackay.com>
2013-03-04 10:50:32 +1300

    Fix check for message media: avoid showing unneeded 'preview message'. Closes #798
    * Message media check was always true. Fix to check media count
    * No preview message only shows when there is something to preview.

--- application/views/admin/messages/main.php
+++ application/views/admin/messages/main.php
@@ -133,7 +133,7 @@
 											<div class="post">
 												<p><?php echo $message_description; ?></p>
 												<?php
-												if ($message_detail OR $message->media != null)
+												if ($message_detail OR $message->media->count() > 0)
 												{
 													?>
 													<p><a href="javascript:preview('message_preview_<?php echo $message_id?>')"><?php echo Kohana::lang('ui_main.preview_message');?></a></p>
