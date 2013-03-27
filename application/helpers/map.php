454aa18fe0029ad42d6a93efed03805f53840d3b
Abbas Adel <abbas.adel@mufix.org>
2013-03-28 00:41:24 +0200

    set Google maps language based on current locale

--- application/helpers/map.php
+++ application/helpers/map.php
@@ -282,7 +282,7 @@ class map_Core {
 		$layer->openlayers = "Google";
 		$layer->title = 'Google Maps Satellite';
 		$layer->description = 'Google Maps Satellite Imagery.';
-		$layer->api_url = 'https://maps.google.com/maps/api/js?v=3.7&amp;sensor=false';
+		$layer->api_url = 'https://maps.google.com/maps/api/js?v=3.7&amp;sensor=false&amp;language='.Kohana::config('locale.language.0');
 		$layer->api_signup = 'http://code.google.com/apis/maps/signup.html';
 		$layer->data = array(
 			'baselayer' => TRUE,
@@ -298,7 +298,7 @@ class map_Core {
 		$layer->openlayers = "Google";
 		$layer->title = 'Google Maps Hybrid';
 		$layer->description = 'Google Maps with roads and terrain.';
-		$layer->api_url = 'https://maps.google.com/maps/api/js?v=3.7&amp;sensor=false';
+		$layer->api_url = 'https://maps.google.com/maps/api/js?v=3.7&amp;sensor=false&amp;language='.Kohana::config('locale.language.0');
 		$layer->api_signup = 'http://code.google.com/apis/maps/signup.html';
 		$layer->data = array(
 			'baselayer' => TRUE,
@@ -306,7 +306,7 @@ class map_Core {
 			'animationEnabled' => TRUE,
 		);
 		$layers[$layer->name] = $layer;
-
+                
 		// GOOGLE Normal
 		$layer = new stdClass();
 		$layer->active = TRUE;
@@ -314,7 +314,7 @@ class map_Core {
 		$layer->openlayers = "Google";
 		$layer->title = 'Google Maps Normal';
 		$layer->description = 'Standard Google Maps Roads';
-		$layer->api_url = 'https://maps.google.com/maps/api/js?v=3.7&amp;sensor=false';
+		$layer->api_url = 'https://maps.google.com/maps/api/js?v=3.7&amp;sensor=false&amp;language='.Kohana::config('locale.language.0');
 		$layer->api_signup = 'http://code.google.com/apis/maps/signup.html';
 		$layer->data = array(
 			'baselayer' => TRUE,
@@ -330,7 +330,7 @@ class map_Core {
 		$layer->openlayers = "Google";
 		$layer->title = 'Google Maps Physical';
 		$layer->description = 'Google Maps Hillshades';
-		$layer->api_url = 'https://maps.google.com/maps/api/js?v=3.7&amp;sensor=false';
+		$layer->api_url = 'https://maps.google.com/maps/api/js?v=3.7&amp;sensor=false&amp;language='.Kohana::config('locale.language.0');
 		$layer->api_signup = 'http://code.google.com/apis/maps/signup.html';
 		$layer->data = array(
 			'baselayer' => TRUE,
