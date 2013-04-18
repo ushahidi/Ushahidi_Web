c6d91453973a63913d8f6c45278f7db35a8f4121
Robbie Mackay <rm@robbiemackay.com>
2013-04-18 14:37:05 +1200

    Make SQL upgrade104-105.sql repeatable
    Use INSERT IGNORE instead of INSERT

--- sql/upgrade104-105.sql
+++ sql/upgrade104-105.sql
@@ -1,14 +1,14 @@
 -- Add twitter api key column
-INSERT INTO `settings` VALUES (' ','twitter_api_key',null);
+INSERT IGNORE INTO `settings` VALUES (' ','twitter_api_key',null);
 
 -- Add twitter api key secret column
-INSERT INTO `settings` VALUES (' ','twitter_api_key_secret',null);
+INSERT IGNORE INTO `settings` VALUES (' ','twitter_api_key_secret',null);
 
 -- Add twitter token column
-INSERT INTO `settings` VALUES (' ','twitter_token',null);
+INSERT IGNORE INTO `settings` VALUES (' ','twitter_token',null);
 
 -- Add twitter token secret column
-INSERT INTO `settings` VALUES (' ','twitter_token_secret',null);
+INSERT IGNORE INTO `settings` VALUES (' ','twitter_token_secret',null);
 
 -- Update DB Version
 UPDATE `settings` SET `value` = 105 WHERE `key` = 'db_version';
