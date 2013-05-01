71da42118f0814926a53a5f9aacd9b486d5d1a81
Robbie Mackay <rm@robbiemackay.com>
2013-05-01 15:27:34 +1200

    Fix double encoding in main reports block

--- themes/default/views/blocks/main_reports.php
+++ themes/default/views/blocks/main_reports.php
@@ -25,7 +25,7 @@
 			$incident_location = $incident->location->location_name;
 		?>
 		<tr>
-			<td><a href="<?php echo url::site() . 'reports/view/' . $incident_id; ?>"> <?php echo html::escape($incident_title) ?></a></td>
+			<td><a href="<?php echo url::site() . 'reports/view/' . $incident_id; ?>"> <?php echo $incident_title ?></a></td>
 			<td><?php echo html::escape($incident_location) ?></td>
 			<td><?php echo $incident_date; ?></td>
 		</tr>
