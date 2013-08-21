68fd906fe7a028916679feeba1140e323ec7942b
Linda Kamau <kamaulynder@gmail.com>
2013-08-22 01:07:25 +0300

    update Changelog

--- sql/upgrade107-108.sql
+++ sql/upgrade107-108.sql
@@ -2,4 +2,4 @@
 UPDATE `settings` SET `value` = 108 WHERE `key` = 'db_version';
 
 -- UPDATE ushahidi_version
-UPDATE `settings` SET `value` = '2.7.1' WHERE `key` = 'ushahidi_version';
+UPDATE `settings` SET `value` = '2.7.1b' WHERE `key` = 'ushahidi_version';
