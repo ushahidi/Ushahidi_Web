303fc194e6ac768f225613392500fd1b723cc6e0
Linda Kamau <kamaulynder@gmail.com>
2013-09-03 19:17:41 +0300

    2.7.1 - Bug Fix Release

--- sql/ushahidi.sql
+++ sql/ushahidi.sql
@@ -1499,4 +1499,4 @@ CREATE TABLE IF NOT EXISTS `verified` (
  *
  */
 UPDATE `settings` SET `value` = '108' WHERE `key` = 'db_version';
-UPDATE `settings` SET `value` = '2.7.1b' WHERE `key`= 'ushahidi_version';
+UPDATE `settings` SET `value` = '2.7.1' WHERE `key`= 'ushahidi_version';
