ffb360e2fe0296f45bb13a91ec07b72a26ea91e6
Diogo Freire <github@diogofreire.com>
2013-09-10 18:17:12 +1200

    Updating update query to add permission to superami

--- sql/upgrade108-109.sql
+++ sql/upgrade108-109.sql
@@ -6,3 +6,9 @@ UPDATE `settings` SET `value` = '2.7.1' WHERE `key` = 'ushahidi_version';
 
 -- Not including id in case current instalation already has a custom permissions.
 INSERT INTO `permissions`(`name`) VALUES ('delete_all_reports');
+
+-- Adding permission to superadmin role - @Robbie: will the ORM pick up on the subqueries?
+INSERT INTO `permission_roles`(`role_id`, `permission_id`) VALUES (
+	(SELECT `id` FROM `roles` WHERE `name` = 'superadmin' LIMIT 1),
+	(SELECT `id` FROM `permission` WHERE `name` = 'delete_all_reports' LIMIT 1),
+);
