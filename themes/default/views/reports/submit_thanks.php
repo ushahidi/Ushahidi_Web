9381b074082117addef2f0189634eb1ba6edc942
Shakhal Levinson <shakhalevinson@gmail.com>
2013-10-22 13:39:40 +0200

    Add link for "submit new report" to reports/submit/thanks #1267

--- themes/default/views/reports/submit_thanks.php
+++ themes/default/views/reports/submit_thanks.php
@@ -2,13 +2,13 @@
 	<div class="content-bg">
 		<!-- start block -->
 		<div class="big-block">
-			<h1><?php echo Kohana::lang('ui_main.reports_submit_new');?></h1>
 			<!-- green-box -->
 			<div class="green-box">
 				<h3><?php echo Kohana::lang('ui_main.reports_submitted');?></h3>
 
 				<div class="thanks_msg"><a href="<?php echo
-					url::site().'reports' ?>"><?php echo Kohana::lang('ui_main.reports_return');?></a><br /><br /><br />
+					url::site().'reports' ?>"><?php echo Kohana::lang('ui_main.reports_return');?></a><br /><br />
+					<a href="<?php echo url::site('reports/submit'); ?>"><?php echo Kohana::lang('ui_main.reports_submit_new');?></a><br /><br />
 					<?php echo Kohana::lang('ui_main.feedback_reports');?><br /><br />
 				</div>
 			</div>
