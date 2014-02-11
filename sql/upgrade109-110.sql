44f783944a5de09a731d510c260bf0320c060ddb
Linda Kamau <kamaulynder@gmail.com>
2014-02-11 14:15:55 +0300

    fixed sql syntax

--- sql/upgrade109-110.sql
+++ sql/upgrade109-110.sql
@@ -7,5 +7,5 @@ INSERT INTO `permissions` (`name`) VALUES ('delete_all_reports');
 -- Adding permission to superadmin role - @Robbie: will the ORM pick up on the subqueries?
 INSERT INTO `permissions_roles` (`role_id`, `permission_id`) VALUES (
 	(SELECT `id` FROM `roles` WHERE `name` = 'superadmin' LIMIT 1),
-	(SELECT `id` FROM `permissions` WHERE `name` = 'delete_all_reports' LIMIT 1),
+	(SELECT `id` FROM `permissions` WHERE `name` = 'delete_all_reports' LIMIT 1)
 );
