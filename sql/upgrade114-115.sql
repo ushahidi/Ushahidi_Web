ed62fae8c9665dcadb87c4bb9b4b2715781edf79
Diogo Freire <github@diogofreire.com>
2014-03-03 12:28:54 +1300

    Re-shuffling sql files

--- sql/upgrade114-115.sql
+++ sql/upgrade114-115.sql
@@ -3,8 +3,5 @@ INSERT INTO `country` (`id`, `iso`, `country`, `capital`, `cities`) VALUES
 (248, 'XK', 'Kosovo', 'Pristina', 0),
 (249, 'SS', 'South Sudan', 'Juba', 0);
 
--- Add feed_geolocation_user as a setting
-INSERT into settings(`key`, `value`) values('feed_geolocation_user', '');
-
 -- Update DB Version --
 UPDATE `settings` SET `value` = '115' WHERE `key` = 'db_version';
