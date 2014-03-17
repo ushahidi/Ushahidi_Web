822cffc6a489a0debd4ce5360a6217adfe916002
Diogo Freire <github@diogofreire.com>
2014-03-17 17:00:41 +1300

    Updating DB version on global config

--- application/config/version.php
+++ application/config/version.php
@@ -9,4 +9,4 @@ $config['ushahidi_version'] = "2.7.2";
  * The Ushahidi Engine DB revision number
  * Increments when changes are made to the Ushahidi DB schema.
  */
-$config['ushahidi_db_version'] = "115";
+$config['ushahidi_db_version'] = "116";
