3b410a207a6617b97aaa5a68d638beea95ba58db
Ugo Sangiorgi <ugo@ugosan.org>
2016-04-21 18:27:07 -0500

    Arreglado menu de categorías y tipos

--- themes/simple-responsive/views/main/layout.php
+++ themes/simple-responsive/views/main/layout.php
@@ -256,96 +256,116 @@ $(function(){
 		</div>
 
 		<!-- menu para desktops -->
+
 		<div class="main-responsive-desktop">
 			<h3><?php echo Kohana::lang('ui_main.category'); ?></h3>
-
-			<nav id="category-responsive-nav" role="navigation">
-
 			<?php
-			foreach ($categories as $category => $category_info)
-			{
-				$category_title = html::escape($category_info[0]);
-				$category_color = $category_info[1];
-				$category_image = ($category_info[2] != NULL)
-					? url::convert_uploaded_to_abs($category_info[2])
-					: NULL;
-				$category_description = html::escape(Category_Lang_Model::category_description($category));
+			// Action::main_sidebar_pre_filters - Add Items to the Entry Page before filters
+			Event::run('ushahidi_action.main_sidebar_pre_filters');
+			?>
 
-				$color_css = 'class="category-icon swatch" style="background-color:#'.$category_color.'"';
-				if ($category_info[2] != NULL)
+			<!-- report category filters -->
+			<div >
+
+				<ul id="category_switch" class="category-filters">
+				<?php
+				$color_css = 'class="category-icon swatch" style="background-color:#'.$default_map_all.'"';
+				$all_cat_image = '';
+				if ($default_map_all_icon != NULL)
 				{
-					$category_image = html::image(array(
-						'src'=>$category_image,
-						));
+					$all_cat_image = html::image(array(
+						'src'=>$default_map_all_icon
+					));
 					$color_css = 'class="category-icon"';
 				}
-
-				echo '<li>'
-					. '<a href="#" id="cat_'. $category .'" title="'.$category_description.'">'
-					. '<span '.$color_css.'>'.$category_image.'</span>'
-					. '<span class="category-title">'.$category_title.'</span>'
-					. '</a>';
-
-				// Get Children
-				echo '<div class="hide" id="child_'. $category .'">';
-				if (sizeof($category_info[3]) != 0)
-				{
-					echo '<ul>';
-					foreach ($category_info[3] as $child => $child_info)
+				?>
+				<li>
+					<a class="active" id="cat_0" href="#">
+						<span <?php echo $color_css; ?>><?php echo $all_cat_image; ?></span>
+						<span class="category-title"><?php echo Kohana::lang('ui_main.all_categories');?></span>
+					</a>
+				</li>
+				<?php
+					foreach ($categories as $category => $category_info)
 					{
-						$child_title = html::escape($child_info[0]);
-						$child_color = $child_info[1];
-						$child_image = ($child_info[2] != NULL)
-							? url::convert_uploaded_to_abs($child_info[2])
+						$category_title = html::escape($category_info[0]);
+						$category_color = $category_info[1];
+						$category_image = ($category_info[2] != NULL)
+							? url::convert_uploaded_to_abs($category_info[2])
 							: NULL;
-						$child_description = html::escape(Category_Lang_Model::category_description($child));
-
-						$color_css = 'class="category-icon swatch" style="background-color:#'.$child_color.'"';
-						if ($child_info[2] != NULL)
+						$category_description = html::escape(Category_Lang_Model::category_description($category));
+						$color_css = 'class="category-icon swatch" style="background-color:#'.$category_color.'"';
+						if ($category_info[2] != NULL)
 						{
-							$child_image = html::image(array(
-								'src' => $child_image
-							));
-
+							$category_image = html::image(array(
+								'src'=>$category_image,
+								));
 							$color_css = 'class="category-icon"';
 						}
-
 						echo '<li>'
-							. '<a href="#" id="cat_'. $child .'" title="'.$child_description.'">'
-							. '<span '.$color_css.'>'.$child_image.'</span>'
-							. '<span class="category-title">'.$child_title.'</span>'
-							. '</a>'
-							. '</li>';
+							. '<a href="#" id="cat_'. $category .'" title="'.$category_description.'">'
+							. '<span '.$color_css.'>'.$category_image.'</span>'
+							. '<span class="category-title">'.$category_title.'</span>'
+							. '</a>';
+						// Get Children
+						echo '<div class="hide" id="child_'. $category .'">';
+						if (sizeof($category_info[3]) != 0)
+						{
+							echo '<ul>';
+							foreach ($category_info[3] as $child => $child_info)
+							{
+								$child_title = html::escape($child_info[0]);
+								$child_color = $child_info[1];
+								$child_image = ($child_info[2] != NULL)
+									? url::convert_uploaded_to_abs($child_info[2])
+									: NULL;
+								$child_description = html::escape(Category_Lang_Model::category_description($child));
+								$color_css = 'class="category-icon swatch" style="background-color:#'.$child_color.'"';
+								if ($child_info[2] != NULL)
+								{
+									$child_image = html::image(array(
+										'src' => $child_image
+									));
+									$color_css = 'class="category-icon"';
+								}
+								echo '<li>'
+									. '<a href="#" id="cat_'. $child .'" title="'.$child_description.'">'
+									. '<span '.$color_css.'>'.$child_image.'</span>'
+									. '<span class="category-title">'.$child_title.'</span>'
+									. '</a>'
+									. '</li>';
+							}
+							echo '</ul>';
+						}
+						echo '</div></li>';
 					}
-					echo '</ul>';
-				}
-				echo '</div></li>';
-			}
-			?>
-			</nav>
-			<div style="padding-top:15px;">
-			<h3><?php echo Kohana::lang('ui_main.type'); ?></h3>
-
-			<nav id="type-responsive-nav" role="navigation">
-				<ul style="padding-left:15px;">
-					<li><a id="media_0" class="active" href="#"><span><?php echo Kohana::lang('ui_main.reports'); ?></span></a></li>
-					<li><a id="media_4" href="#"><span><?php echo Kohana::lang('ui_main.news'); ?></span></a></li>
-					<li><a id="media_1" href="#"><span><?php echo Kohana::lang('ui_main.pictures'); ?></span></a></li>
-					<li><a id="media_2" href="#"><span><?php echo Kohana::lang('ui_main.video'); ?></span></a></li>
-					<li><a id="media_0" href="#"><span><?php echo Kohana::lang('ui_main.all'); ?></span></a></li>
+				?>
 				</ul>
-			</nav>
+				<!-- / category filters -->
 
+			</div>
+			<!-- / report category filters -->
 
-				<div class="floatbox">
-						<?php
-						// Action::main_filters - Add items to the main_filters
-						Event::run('ushahidi_action.map_main_filters');
-						?>
-					</div>
-					<!-- / report type filters -->
+			<!-- report type filters -->
+			<div id="report-type-filter" class="filters">
+				<h3><?php echo Kohana::lang('ui_main.type'); ?></h3>
+					<ul>
+						<li><a id="media_0" class="active" href="#"><span><?php echo Kohana::lang('ui_main.reports'); ?></span></a></li>
+						<li><a id="media_4" href="#"><span><?php echo Kohana::lang('ui_main.news'); ?></span></a></li>
+						<li><a id="media_1" href="#"><span><?php echo Kohana::lang('ui_main.pictures'); ?></span></a></li>
+						<li><a id="media_2" href="#"><span><?php echo Kohana::lang('ui_main.video'); ?></span></a></li>
+						<li><a id="media_0" href="#"><span><?php echo Kohana::lang('ui_main.all'); ?></span></a></li>
+					</ul>
+					<div class="floatbox">
+							<?php
+							// Action::main_filters - Add items to the main_filters
+							Event::run('ushahidi_action.map_main_filters');
+							?>
+						</div>
+						<!-- / report type filters -->
 			</div>
 
+			
 			<!-- additional content -->
 			<?php
 			if (Kohana::config('settings.allow_reports'))
