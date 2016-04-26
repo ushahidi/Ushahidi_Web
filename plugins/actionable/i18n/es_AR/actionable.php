c7ee98837f2b90e7009534956597155b64b7f58b
Jonathan Reyes <j@jreyes.org>
2016-04-25 20:09:03 -0500

    Agrega traducción del plugin actionable desde nuestro fork desastre-ecuador/Ushahidi-plugin-actionable

---
+++ plugins/actionable/i18n/es_AR/actionable.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+	$lang = array(
+		'actionable' => 'Procesable',
+		'not_actionable' => 'No procesable',
+		'urgent' => 'Urgente',
+		'action_taken' => 'Acción tomada',
+		'action_status_description' => 'Este reporte necesita alguna acción?',
+		'action_summary_description' => 'Ha sido tomada alguna acción? Resumir la acción tomada o requerida',
+		'summary' => 'Resumen',
+		'action_needed' => 'Acción necesaria',
+		'action_urgent' => 'Es necesario actuar urgentemente'
+	);
+?>
